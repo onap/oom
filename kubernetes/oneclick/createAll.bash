339f782fd91c84a18eb5426c1faf5f07436b6a36
Munir Ahmad <munir.ahmad@bell.ca>
2017-08-09 14:51:11 -0400

    multiple ONAP instances in the same k8s env
    Change-Id: I77e48e635d20295aee2b351bbf94203ed47fe5b0
    Signed-off-by: Munir Ahmad <munir.ahmad@bell.ca>

--- kubernetes/oneclick/createAll.bash
+++ kubernetes/oneclick/createAll.bash
@@ -6,6 +6,7 @@ Usage: $0 [PARAMs]
 -u                  : Display usage
 -n [NAMESPACE]      : Kubernetes namespace (required)
 -s false            : Exclude services (default: true)
+-i [INSTANCE]       : ONAP deployment instance # (default: 1)
 -a [APP]            : Specify a specific ONAP component (default: all)
                       from the following choices:
                       sdc, aai ,mso, message-router, robot,
@@ -18,15 +19,21 @@ create_namespace() {
 }
 
 create_service() {
+  sed -i -- 's/nodePort: [0-9]\{2\}[02468]\{1\}/nodePort: '"$3"'/g' ../$2/all-services.yaml
+  sed -i -- 's/nodePort: [0-9]\{2\}[13579]\{1\}/nodePort: '"$4"'/g' ../$2/all-services.yaml
   kubectl --namespace $1-$2 create -f ../$2/all-services.yaml
+  mv ../$2/all-services.yaml-- ../$2/all-services.yaml
 }
+
 
 #MAINs
 NS=
 INCL_SVC=true
 APP=
+INSTANCE=1
+MAX_INSTANCE=5
 
-while getopts ":n:u:s:a:" PARAM; do
+while getopts ":n:u:s:i:a:" PARAM; do
   case $PARAM in
     u)
       usage
@@ -37,6 +44,9 @@ while getopts ":n:u:s:a:" PARAM; do
       ;;
     s)
       INCL_SVC=${OPTARG}
+      ;;
+    i)
+      INSTANCE=${OPTARG}
       ;;
     a)
       APP=${OPTARG}
@@ -63,6 +73,20 @@ else
   ONAP_APPS=('sdc' 'aai' 'mso' 'message-router' 'robot' 'vid' 'sdnc' 'portal' 'policy' 'appc')
 fi
 
+if [[ "$INCL_SVC" == true ]]; then
+
+  if [ "$INSTANCE" -gt "$MAX_INSTANCE" ];then
+    printf "\n********** You choose to create ${INSTANCE}th instance of ONAP \n"
+    printf "\n********** Due to port allocation only ${MAX_INSTANCE} instances of ONAP is allowed per kubernetes deployment\n"
+    exit 1
+  fi
+
+  start=$((300+2*INSTANCE))
+  end=$((start+1))
+  printf "\n********** Creating instance ${INSTANCE} of ONAP with port range ${start}00 and ${end}99\n"
+
+fi
+
 printf "\n********** Creating up ONAP: ${ONAP_APPS[*]}\n"
 
 for i in ${ONAP_APPS[@]}; do
@@ -71,7 +95,7 @@ for i in ${ONAP_APPS[@]}; do
 
   if [[ "$INCL_SVC" == true ]]; then
     printf "\nCreating services **********\n"
-    create_service $NS $i
+    create_service $NS $i $start $end
   fi
 
   printf "\n"
@@ -82,4 +106,4 @@ for i in ${ONAP_APPS[@]}; do
   /bin/bash $i.sh $NS $i 'create'
 done
 
-printf "**** Done ****"
+printf "\n**** Done ****\n"
