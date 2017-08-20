f8d6ed7a64a016413d8b0d800045b10ab669b8c8
kerenj <keren.joseph@amdocs.com>
2017-08-20 16:56:31 +0000

    [OOM-52]-Helm Parameterization
    change k8s deployment to support helm parameterization
    Issue-ID: OOM-52
    Change-Id: I584852a23ec47fcc876f1471752f73052698f4cf
    Signed-off-by: kerenj <keren.joseph@amdocs.com>

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -11,13 +11,13 @@ The following is an example of creating the first deployed instance in a K8s
 cluster. The name given to the instance is 'dev1'. This will serve as the
 Namespace prefix for each deployed ONAP component (ie. dev1-mso).
 
-  1. oom/kubernetes/config/createConfig.sh -n dev1
+  1. oom/kubernetes/helm/config/createConfig.sh -n dev1
 
-  2. oom/kubernetes/oneclick/createAll.bash -n dev1
+  2. oom/kubernetes/helm/oneclick/createAll.bash -n dev1
 
 To delete the runtime containers for the deployed instance, use the following:
 
-  3. oom/kubernetes/oneclick/deleteAll.bash -n dev1
+  3. oom/kubernetes/helm/oneclick/deleteAll.bash -n dev1
 
 Note that deleting the runtime containers does not remove the configuration
 created in step 1.
@@ -30,6 +30,6 @@ that are running inside a Kubernetes cluster.
 
 Example if this is the 2 instance of an ONAP deployment in the cluster:
 
-  1. oom/kubernetes/config/createConfig.sh -n test
+  1. oom/kubernetes/helm/config/createConfig.sh -n test
 
-  2. oom/kubernetes/oneclick/createAll.bash -n test -i 2
+  2. oom/kubernetes/helm/oneclick/createAll.bash -n test -i 2
---
+++ kubernetes/helm/oneclick/deleteAll.bash
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+. $(dirname "$0")/setenv.bash
+
+delete_namespace() {
+  _NS=$1-$2
+  kubectl delete namespace $_NS
+  printf "Waiting for namespace $_NS termination...\n"
+  while kubectl get namespaces $_NS > /dev/null 2>&1; do
+    sleep 2
+  done
+  printf "Namespace $_NS deleted.\n\n"
+}
+
+delete_registry_key() {
+  kubectl --namespace $1-$2 delete secret onap-docker-registry-key
+}
+
+delete_onap_helm() {
+  helm delete $1 --purge
+}
+
+usage() {
+  cat <<EOF
+Usage: $0 [PARAMs]
+-u                  : Display usage
+-n [NAMESPACE]      : Kubernetes namespace (required)
+-s true             : Include services (default: false)
+-a [APP]            : Specify a specific ONAP component (default: all)
+                      from the following choices:
+                      sdc, aai ,mso, message-router, robot,
+                      vid, sdnc, portal, policy, appc
+EOF
+}
+
+#MAINs
+NS=
+INCL_SVC=false
+APP=
+
+while getopts ":n:u:s:a:" PARAM; do
+  case $PARAM in
+    u)
+      usage
+      exit 1
+      ;;
+    n)
+      NS=${OPTARG}
+      ;;
+    a)
+      APP=${OPTARG}
+      if [[ -z $APP ]]; then
+        usage
+        exit 1
+      fi
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+  esac
+done
+
+if [[ -z $NS ]]; then
+  usage
+  exit 1
+fi
+
+if [[ ! -z "$APP" ]]; then
+  ONAP_APPS=($APP)
+fi
+
+printf "\n********** Cleaning up ONAP: ${ONAP_APPS[*]}\n"
+
+for i in ${ONAP_APPS[@]}; do
+
+  printf "\nDeleting deployments and services **********\n"
+  delete_onap_helm $i
+  delete_namespace $NS $i
+
+done
+
+
+printf "\n********** Gone **********\n"
+
