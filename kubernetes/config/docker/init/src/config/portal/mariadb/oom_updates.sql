82c63d33cf8ade806228a9e5ae43dd4527789a91
Mandeep Khinda <mandeep.khinda@amdocs.com>
2017-08-21 16:04:37 -0400

    ONAP Portal 1.1 work
    -Updating yamls, and config pod data and tag
    -VNC pod is currently WIP as the other services
    are not available yet.
    
    Issue-ID: OOM-151
    
    Change-Id: I8f201645481888657d42bfe0e16ca505187521c7
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

---
+++ kubernetes/config/docker/init/src/config/portal/mariadb/oom_updates.sql
@@ -0,0 +1,7 @@
+USE portal;
+/*
+Any updates required by OOM to the portaldb are made here.
+1. split up SDC-FE and SDC-BE.  Originally both FE and BE point to the same IP
+while the OOM K8s version has these service split up.
+*/
+UPDATE fn_app SET app_rest_endpoint = 'http://sdc.api.be.simpledemo.openecomp.org:8080/api/v2' where app_name = 'SDC';
