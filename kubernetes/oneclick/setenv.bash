3b697f6c07a1af4625f3982096e87f2c783f8890
kerenj <keren.joseph@amdocs.com>
2017-08-23 11:21:21 +0000

    policy K8S-Helm Parameterization
    changed policy k8s deployment to support helm parameterization. updated images to 1.1
    Issue-ID: OOM-52
    Change-Id: I4eb0626c7c00ace2c266d6ddbb1313f3b654d69e
    Signed-off-by: kerenj <keren.joseph@amdocs.com>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal')
-ONAP_APPS=('sdc' 'aai' 'policy' 'appc')
+HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy')
+ONAP_APPS=('sdc' 'aai' 'appc')
 ONAP_DOCKER_REGISTRY_KEY=${ONAP_DOCKER_REGISTRY_KEY:-onap-docker-registry-key}
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
