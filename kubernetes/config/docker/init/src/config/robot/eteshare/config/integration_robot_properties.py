cca0d796bd534598d561281ee829824f8cbbd65a
Ahmed Alabulrahman <ahmed.alabdulrahman@amdocs.com>
2017-08-23 13:33:10 -0400

    Updated robot yamls and configs to 1.1
    Changed yamls to pull 1.1 image and updated necessary configs
    
    Issue-ID: OOM-169
    
    Change-Id: Ie747105b291b88d1d6d54f6e717dceb6310bf438
    Signed-off-by: Ahmed Alabulrahman <ahmed.alabdulrahman@amdocs.com>

--- kubernetes/config/docker/init/src/config/robot/eteshare/config/integration_robot_properties.py
+++ kubernetes/config/docker/init/src/config/robot/eteshare/config/integration_robot_properties.py
@@ -4,13 +4,17 @@ GLOBAL_AAF_USERNAME = "dgl@openecomp.org"
 GLOBAL_AAF_PASSWORD = "ecomp_admin"
 # aai info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_AAI_SERVER_URL = "https://aai-service.onap-aai:8443"
+AAI_FRONTEND_ENDPOINT = "https://aai-service.onap-aai:8443"
 GLOBAL_AAI_USERNAME = "AAI"
 GLOBAL_AAI_PASSWORD = "AAI"
 # appc info - everything is from the private oam network (also called ecomp private network)
-GLOBAL_APPC_SERVER = "http://sdnhost.onap-appc:8282"
+GLOBAL_APPC_SERVER = "http://10.0.2.1:8282"
+APPC_ENDPOINT = "http://10.0.2.1:8282"
 GLOBAL_APPC_USERNAME = "admin"
 GLOBAL_APPC_PASSWORD = "Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U"
 # sdc info - everything is from the private oam network (also called ecomp private network)
+ASDC_BE_ENDPOINT = "http://sdc-be.onap-sdc"
+ASDC_FE_ENDPOINT = "http://sdc-fe.onap-sdc"
 GLOBAL_ASDC_FE_SERVER = "http://sdc-fe.onap-sdc"
 GLOBAL_ASDC_BE_SERVER = "http://sdc-be.onap-sdc"
 GLOBAL_ASDC_FE_PORT = "8181"
@@ -19,16 +23,20 @@ GLOBAL_ASDC_BE_USERNAME = "beep"
 GLOBAL_ASDC_BE_PASSWORD = "boop"
 # dcae info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_DCAE_SERVER = "http://dcae-controller.onap-dcae:9998"
+DCAE_ENDPOINT = "http://dcae-controller.onap-dcae:9998"
 GLOBAL_DCAE_USERNAME = "console"
 GLOBAL_DCAE_PASSWORD = "ZjJkYjllMjljMTI2M2Iz"
 # message router info - everything is from the private oam network (also called ecomp private network)
+MR_ENDPOINT = "http://dmaap.onap-message-router:3904"
 GLOBAL_MR_SERVER = "http://dmaap.onap-message-router:3904"
 # mso info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_MSO_SERVER = "http://mso.onap-mso:8080"
+MSO_ENDPOINT = "http://mso.onap-mso:8080"
 GLOBAL_MSO_USERNAME = "InfraPortalClient"
 GLOBAL_MSO_PASSWORD = "password1$"
 # openstack info - info to select right info in environment
-GLOBAL_OPENSTACK_KEYSTONE_SERVER = "http://OPENSTACK_KEYSTONE_IP_HERE:5000"
+GLOBAL_OPENSTACK_KEYSTONE_SERVER = "http://192.168.141.233:5000"
+GLOBAL_OPENSTACK_TENANT_NAME = "onap"
 # packet generate vnf info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_PACKET_GENERATOR_PORT = "8183"
 GLOBAL_PACKET_GENERATOR_USERNAME = "admin"
@@ -36,13 +44,16 @@ GLOBAL_PACKET_GENERATOR_PASSWORD = "admin"
 GLOBAL_PGN_PORT = "2831"
 # policy info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_POLICY_SERVER_URL = "http://pypdp.onap-policy:8480"
-GLOBAL_POLICY_AUTH = "dGVzdHJlc3Q6M2MwbXBVI2gwMUBOMWMz"
+POLICY_ENDPOINT = "http://pypdp.onap-policy:8480"
+GLOBAL_POLICY_AUTH = "dGVzdHBkcDphbHBoYTEyMw=="
 GLOBAL_POLICY_CLIENTAUTH = "cHl0aG9uOnRlc3Q="
 GLOBAL_POLICY_HEALTHCHECK_URL = "http://drools.onap-policy:6969"
+POLICY_HEALTHCHECK_ENDPOINT = "http://drools.onap-policy:6969"
 GLOBAL_POLICY_USERNAME = "healthcheck"
 GLOBAL_POLICY_PASSWORD = "zb!XztG34"
 # portal info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_PORTAL_SERVER = "http://portalapps.onap-portal:8989"
+PORTAL_ENDPOINT = "http://portalapps.onap-portal:8989"
 GLOBAL_PORTAL_USERNAME = "demo"
 GLOBAL_PORTAL_PASSWORD = "Kp8bJ4SXszM0WXlhak3eHlcse"
 # sdngc info - everything is from the private oam network (also called ecomp private network)
@@ -58,10 +69,12 @@ GLOBAL_VID_PASSWORD = "Kp8bJ4SXszM0WX"
 GLOBAL_VID_HEALTH_USERNAME = "Default"
 GLOBAL_VID_HEALTH_PASSWORD = "AppPassword!1"
 GLOBAL_VID_SERVER = "http://vid-server.onap-vid:8080"
+VID_ENDPOINT = "http://vid-server.onap-vid:8080"
 #global selenium info
 GLOBAL_PROXY_WARNING_TITLE=""
 GLOBAL_PROXY_WARNING_CONTINUE_XPATH=""
 # settings for vm to attach vvg too
-GLOBAL_VVGSERVER_IMAGE = "UBUNTU_14_IMAGE_NAME_HERE"
-GLOBAL_VVGSERVER_FLAVOR = "OPENSTACK_FLAVOUR_NAME_HERE"
+GLOBAL_VVGSERVER_IMAGE = "Ubuntu 14.04 LTS (Trusty Tahr) (PVHVM)"
+GLOBAL_VVGSERVER_FLAVOR = "4 GB General Purpose v1"
 # dns info
+# GLOBAL_DNS_TRAFFIC_DURATION = "600"
