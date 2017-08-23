b506fa2de8aaf76ff1a70abb636b42f7841c49f4
kerenj <keren.joseph@amdocs.com>
2017-08-23 12:46:02 +0000

    robot K8S-Helm Parameterization
    changed robot k8s deployment to support helm parameterization. updated image to 1.1
    Issue-ID: OOM-52
    Change-Id: Ibd7cd5a73589abb42940897a5a524cb44f53dd98
    Signed-off-by: kerenj <keren.joseph@amdocs.com>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HELM_APPS=('mso')
-ONAP_APPS=('sdc' 'aai' 'message-router' 'robot' 'vid' 'sdnc' 'portal' 'policy' 'appc')
+HELM_APPS=('mso' 'message-router' 'sdnc' 'robot')
+ONAP_APPS=('sdc' 'aai' 'vid' 'portal' 'policy' 'appc')
 ONAP_DOCKER_REGISTRY_KEY=${ONAP_DOCKER_REGISTRY_KEY:-onap-docker-registry-key}
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
