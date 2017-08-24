ed1e3b013339b962326d6a15bb1e132875b930f9
Itay Hassid <itay.hassid@amdocs.com>
2017-08-24 12:54:42 +0000

    Seed of deployment of log components
    Deployment of: Logstash + ElasticSearch + Kubana with all the configuration files
    Issue-ID:OOM-110
    
    Change-Id: I1788485ccb283f0ec4dc8910479072a6cf034e5f
    Signed-off-by: Itay Hassid <itay.hassid@amdocs.com>

--- kubernetes/oneclick/setenv.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc')
-ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
-ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
-ONAP_DOCKER_PASS=${ONAP_DOCKER_PASS:-docker}
-ONAP_DOCKER_MAIL=${ONAP_DOCKER_MAIL:-$USERNAME@$USERDOMAIN}
