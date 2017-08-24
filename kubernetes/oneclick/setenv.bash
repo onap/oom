fdc176253250ac0100a15b0d08fe752e2b5cb701
kerenj <keren.joseph@amdocs.com>
2017-08-24 11:32:22 +0000

    aai K8S-Helm Parameterization
    changed aai k8s deployment to support helm parameterization.
    Issue-ID: OOM-52
    Change-Id: Ifffdb4204a3fdb2c7be62063134c489f8123012d
    Signed-off-by: kerenj <keren.joseph@amdocs.com>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc')
-ONAP_APPS=('sdc' 'aai')
+HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai')
+ONAP_APPS=('sdc')
 ONAP_DOCKER_REGISTRY_KEY=${ONAP_DOCKER_REGISTRY_KEY:-onap-docker-registry-key}
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
