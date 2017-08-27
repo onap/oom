d019a3945e94ecb9698a112c1af83f46c9332fc6
yuryn <Yury.Novitsky@Amdocs.com>
2017-08-27 20:25:29 +0300

    Add AAI haproxy
    1. Add AAI haproxy
    2. Create aai-resources and aai-traversal log dirs
    
    Change-Id: Ib60cc7e482cafe2f1185db7bead181f929165ed3
    Issue-ID: OOM-65
    Signed-off-by: yuryn <Yury.Novitsky@Amdocs.com>
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

--- kubernetes/config/docker/init/config-init.sh
+++ kubernetes/config/docker/init/config-init.sh
@@ -20,18 +20,28 @@ mkdir -p /config-init/$NAMESPACE/sdc/logs/ASDC/ASDC-KB/
 mkdir -p /config-init/$NAMESPACE/sdc/logs/ASDC/ASDC-BE/
 mkdir -p /config-init/$NAMESPACE/sdc/logs/ASDC/ASDC-FE/
 mkdir -p /config-init/$NAMESPACE/aai/opt/aai/logroot/
-mkdir -p /config-init/$NAMESPACE/aai/aai-resources/
+mkdir -p /config-init/$NAMESPACE/aai/model-loader/logs/
+mkdir -p /config-init/$NAMESPACE/aai/haproxy/log/
+mkdir -p /config-init/$NAMESPACE/aai/aai-traversal/logs/ajsc-jetty/gc/
+mkdir -p /config-init/$NAMESPACE/aai/aai-traversal/logs/dmaapAAIEventConsumer/
+mkdir -p /config-init/$NAMESPACE/aai/aai-traversal/logs/perf-audit/
+mkdir -p /config-init/$NAMESPACE/aai/aai-traversal/logs/rest/
+mkdir -p /config-init/$NAMESPACE/aai/aai-resources/logs/ajsc-jetty/gc/
+mkdir -p /config-init/$NAMESPACE/aai/aai-resources/logs/dmaapAAIEventConsumer/
+mkdir -p /config-init/$NAMESPACE/aai/aai-resources/logs/perf-audit/
+mkdir -p /config-init/$NAMESPACE/aai/aai-resources/logs/rest/
 mkdir -p /config-init/$NAMESPACE/aai/sparky-be/logs/
-mkdir -p /config-init/$NAMESPACE/aai/aai-traversal/logs/
 
 chmod -R 777 /config-init/$NAMESPACE/sdc/logs/
 chmod -R 777 /config-init/$NAMESPACE/portal/logs/
 chmod -R 777 /config-init/$NAMESPACE/aai/aai-config/
 chmod -R 777 /config-init/$NAMESPACE/aai/aai-data/
 chmod -R 777 /config-init/$NAMESPACE/aai/opt/aai/logroot/
-chmod -R 777 /config-init/$NAMESPACE/aai/aai-resources/
-chmod -R 777 /config-init/$NAMESPACE/aai/sparky-be/logs/
+chmod -R 777 /config-init/$NAMESPACE/aai/model-loader/logs/
+chmod -R 777 /config-init/$NAMESPACE/aai/haproxy/log/
 chmod -R 777 /config-init/$NAMESPACE/aai/aai-traversal/logs/
+chmod -R 777 /config-init/$NAMESPACE/aai/aai-resources/logs/
+chmod -R 777 /config-init/$NAMESPACE/aai/sparky-be/logs/
 
 
 # replace the default 'onap' namespace qualification of K8s hostnames within the config files
