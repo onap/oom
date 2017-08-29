add81fb24ef2c9a20bdd37d2c904effbc1f958ae
Michael O'Brien <frank.obrien@amdocs.com>
2017-08-29 10:41:31 -0400

    dcae wip seed code - dcae.sh
    Issue-ID: OOM-5
    Change-Id: Ia6a7e731508071415f9fdcaed26f5a5eab4435e4
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>

--- kubernetes/oneclick/dcae.sh
+++ kubernetes/oneclick/dcae.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo "DCAE under construction"
+# manual deployment order is mentioned - but we need to specify dependencies in the service
+KUBECTL_CMD="kubectl --namespace $1-$2 $3 -f ../$2"
+
+printf "dcae....\n"
+$KUBECTL_CMD/message-router/dcae-zookeeper.yaml
+$KUBECTL_CMD/message-router/dcae-kafka.yaml
+$KUBECTL_CMD/message-router/dcae-dmaap.yaml
+
+$KUBECTL_CMD/pgaas/pgaas.yaml
+
+$KUBECTL_CMD/dcae-collector-common-event.yaml
+$KUBECTL_CMD/dcae-collector-dmaapbc.yaml
+$KUBECTL_CMD/dcae-collector-pvs.yaml
+
+$KUBECTL_CMD/cdap/cdap0-dep.yaml
+$KUBECTL_CMD/cdap/cdap1-dep.yaml
+$KUBECTL_CMD/cdap/cdap2-dep.yaml
