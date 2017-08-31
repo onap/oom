2ab32e2f5f4140df9267deba72d9dbe61893b78c
Eric Au <kachun.au@amdocs.com>
2017-08-31 16:18:11 -0400

    [WIP] Policy 1.1 in Kubernetes
    - up verion containers to use 1.1
    - pap and brmsgw report it is waiting for pap 9091 port to be open but pdp and pypdp reports pap 9091 port is open
    
    Change-Id: Ibb685ec603a59dba56aa9673044ae1923d993191
    Issue-Id: OOM-249
    Signed-off-by: Eric Au <kachun.au@amdocs.com>

--- kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/push-policies.sh
+++ kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/push-policies.sh
@@ -12,7 +12,7 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "vFirewall",
   "policyScope": "com",
   "policyType": "MicroService"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
 
 sleep 2
 
@@ -21,7 +21,7 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "vLoadBalancer",
   "policyScope": "com",
   "policyType": "MicroService"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy' 
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
 
 sleep 2
 curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/plain' --header 'ClientAuth: cHl0aG9uOnRlc3Q=' --header 'Authorization: Basic dGVzdHJlc3Q6M2MwbXBVI2gwMUBOMWMz' --header 'Environment: TEST' -d '{
@@ -29,7 +29,7 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "BRMSParamvLBDemoPolicy",
   "policyScope": "com",
   "policyType": "BRMS_Param"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
 
 sleep 2
 
@@ -38,7 +38,7 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "BRMSParamvFWDemoPolicy",
   "policyScope": "com",
   "policyType": "BRMS_Param"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
 
 sleep 2
 
@@ -46,7 +46,7 @@ curl -X DELETE --header 'Content-Type: application/json' --header 'Accept: text/
 "pdpGroup": "default",
 "policyComponent": "PDP",
 "policyName": "com.Config_BRMS_Param_BRMSParamvFWDemoPolicy.1.xml"
-}' 'http://pypdp:8480/PyPDPServer/deletePolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/deletePolicy'
 
 
 
@@ -54,7 +54,7 @@ curl -X DELETE --header 'Content-Type: application/json' --header 'Accept: text/
 "pdpGroup": "default",
 "policyComponent": "PDP",
 "policyName": "com.Config_BRMS_Param_BRMSParamvLBDemoPolicy.1.xml"
-}' 'http://pypdp:8480/PyPDPServer/deletePolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/deletePolicy'
 
 sleep 2
 curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/plain' --header 'ClientAuth: cHl0aG9uOnRlc3Q=' --header 'Authorization: Basic dGVzdHJlc3Q6M2MwbXBVI2gwMUBOMWMz' --header 'Environment: TEST' -d '{
@@ -62,7 +62,7 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "BRMSParamvLBDemoPolicy",
   "policyScope": "com",
   "policyType": "BRMS_Param"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy'
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
 
 sleep 2
 
@@ -71,5 +71,4 @@ curl -X PUT --header 'Content-Type: application/json' --header 'Accept: text/pla
   "policyName": "BRMSParamvFWDemoPolicy",
   "policyScope": "com",
   "policyType": "BRMS_Param"
-}' 'http://pypdp:8480/PyPDPServer/pushPolicy'
-
+}' 'http://pypdp.onap-policy:8480/PyPDPServer/pushPolicy'
