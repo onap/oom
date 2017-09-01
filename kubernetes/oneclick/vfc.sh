571207476d000416b6b67f5f89b87c6b9f979c78
maopengzhang <zhang.maopeng1@zte.com.cn>
2017-09-01 09:37:48 +0800

    Add VFC containers to ONAP K8S
    Add VFC containers to ONAP K8S
    
    MK:- fixed issues related to port names longer than 15chars
       - scoped pods to the onap-vfc namespace.
    All deployments still fail due to missing docker images in nexus3
    
    Yan:- fixed docker images in nexus3
    
    Change-Id: Iab9f641aaa580511c0e4a073a71da723aaf07315
    Issue-ID: OOM-19
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

---
+++ kubernetes/oneclick/vfc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KUBECTL_CMD="kubectl --namespace $1-$2 $3 -f ../$2"
+
+printf "vfc....\n"
+#$KUBECTL_CMD/vfc-activiti-deployment.yaml
+$KUBECTL_CMD/vfc-catalog-deployment.yaml
+$KUBECTL_CMD/vfc-emsdriver-deployment.yaml
+$KUBECTL_CMD/vfc-gvnfmdriver-deployment.yaml
+$KUBECTL_CMD/vfc-hwvnfmdriver-deployment.yaml
+$KUBECTL_CMD/vfc-jujudriver-deployment.yaml
+$KUBECTL_CMD/vfc-nslcm-deployment.yaml
+$KUBECTL_CMD/vfc-resmgr-deployment.yaml
+$KUBECTL_CMD/vfc-vnflcm-deployment.yaml
+$KUBECTL_CMD/vfc-vnfmgr-deployment.yaml
+$KUBECTL_CMD/vfc-vnfres-deployment.yaml
+$KUBECTL_CMD/vfc-workflow-deployment.yaml
+$KUBECTL_CMD/vfc-ztesdncdriver-deployment.yaml
+$KUBECTL_CMD/vfc-ztevmanagerdriver-deployment.yaml
