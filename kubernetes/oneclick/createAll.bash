08db3aef13e70ed9284e7d590ccddddc0e296571
tzvika avni <tzvikaa@amdocs.com>
2017-09-07 19:43:57 +0300

    deploy components from different path
    Provide ability to deploy components in a different path via createAll.sh
    Issue-ID: OOM-291
    
    Change-Id: Ia5f0c5e16bf3f9edff6c457176b6f428603d9544
    Signed-off-by: tzvika avni <tzvikaa@amdocs.com>

--- kubernetes/oneclick/createAll.bash
+++ kubernetes/oneclick/createAll.bash
@@ -8,6 +8,7 @@ usage() {
 Usage: $0 [PARAMs]
 -u                  : Display usage
 -n [NAMESPACE]      : Kubernetes namespace (required)
+-l [LOCATION]       : Location of oom project
 -i [INSTANCE]       : ONAP deployment instance # (default: 1)
 -a [APP]            : Specify a specific ONAP component (default: all)
                       from the following choices:
@@ -25,12 +26,17 @@ create_registry_key() {
 }
 
 create_onap_helm() {
-  helm install ../$2/ --name $1-$2 --namespace $1 --set nsPrefix=$1 --set nodePortPrefix=$3
+  prefix="../"
+  if [[ ! -z $4 ]]; then
+    prefix=$4
+  fi
+  helm install $4/$2/ --name $1-$2 --namespace $1 --set nsPrefix=$1 --set nodePortPrefix=$3
 }
 
 
 #MAINs
 NS=
+LOCATION=
 INCL_SVC=true
 APP=
 INSTANCE=1
@@ -39,7 +45,7 @@ DU=$ONAP_DOCKER_USER
 DP=$ONAP_DOCKER_PASS
 _FILES_PATH=$(echo ../$i/templates)
 
-while getopts ":n:u:s:i:a:du:dp:" PARAM; do
+while getopts ":n:u:s:i:a:du:dp:l:" PARAM; do
   case $PARAM in
     u)
       usage
@@ -50,6 +56,9 @@ while getopts ":n:u:s:i:a:du:dp:" PARAM; do
       ;;
     i)
       INSTANCE=${OPTARG}
+      ;;
+    l)
+      LOCATION=${OPTARG}
       ;;
     a)
       APP=${OPTARG}
@@ -106,7 +115,7 @@ for i in ${HELM_APPS[@]}; do
   create_registry_key $NS $i ${NS}-docker-registry-key $ONAP_DOCKER_REGISTRY $DU $DP $ONAP_DOCKER_MAIL
 
   printf "\nCreating deployments and services **********\n"
-  create_onap_helm $NS $i $start
+  create_onap_helm $NS $i $start $LOCATION
 
   printf "\n"
 done
