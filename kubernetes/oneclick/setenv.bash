c80b794569a7c45bf8cf885e8ccd1fc3b367e028
liangke <lokyse@163.com>
2017-09-12 15:41:20 +0800

    Add Multicloud containers to ONAP K8S
    Change-Id: I0801156481c8306cc7599a27d50b981385968a70
    Issue-Id: OOM-18
    Signed-off-by: liangke <lokyse@163.com>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc')
+HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc' 'multicloud')
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
 ONAP_DOCKER_PASS=${ONAP_DOCKER_PASS:-docker}
