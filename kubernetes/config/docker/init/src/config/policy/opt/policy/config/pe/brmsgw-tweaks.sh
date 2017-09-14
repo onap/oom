126102491526b5ba06092855b84ffe359b117df0
Mandeep Khinda <mandeep.khinda@amdocs.com>
2017-09-14 11:37:33 +0000

    move policy config to onap 1.1
    was the cause of the robot failure for policy in oom
    
    Issue-ID: OOM-297
    
    Change-Id: Iabb24cc7a007ef852cde3ebe2cbb9226bb4153b3
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>
    Signed-off-by: Ahmed Alabulrahman <ahmed.alabdulrahman@amdocs.com>

--- kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/brmsgw-tweaks.sh
+++ kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/brmsgw-tweaks.sh
@@ -13,14 +13,26 @@ fi
 
 source "${POLICY_HOME}/etc/build.info"
 
+if [ -z "${version}" ]; then
+	echo "error: no version information present"
+	exit 1
+fi
+
 for CONFIG in ${PROPS_RUNTIME} ${PROPS_INSTALL}; do
 	if [ ! -f "${CONFIG}" ]; then
 		echo "warning: configuration does not exist: ${CONFIG}"
 	else
-		if [ -n "${version}" ]; then
-			/bin/sed -i -e "s/brms.dependency.version=.*/brms.dependency.version=${version}/g" "${CONFIG}"
-		else
-			echo "error: no version information present"
-		fi
+		sed -i -e "s/brms.dependency.version=.*/brms.dependency.version=${version}/g" "${CONFIG}"
+	fi
+done
+
+DEPS_JSON_RUNTIME="${POLICY_HOME}/servers/brmsgw/dependency.json"
+DEPS_JSON_INSTALL="${POLICY_HOME}/install/servers/brmsgw/dependency.json"
+
+for DEP in ${DEPS_JSON_RUNTIME} ${DEPS_JSON_INSTALL}; do
+	if [ ! -f "${DEP}" ]; then
+		echo "warning: configuration does not exist: ${DEP}"
+	else
+		sed -i -e "s/\"version\":.*/\"version\": \"${version}\"/g" "${DEP}"
 	fi
 done
--- kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/pap-tweaks.sh
+++ kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/pap-tweaks.sh
@@ -1,15 +1,1 @@
 #! /bin/bash
-
-# config directory may contain an ip_addr.txt file that specifies
-# the VM IP address.  Substitute this value in the URL in the
-# config.json file, overriding the hostname that came from the
-# REST_PAPURL_WITH_AUTH_PASSWORD property in console.conf. This is
-# to avoid hardcoding an IP address in console.conf that can change
-# from one VM instance to the next.
-
-if [[ -f config/ip_addr.txt ]]; then
-	vm_ip=$(<config/ip_addr.txt)
-	echo "Substituting VM IP address $vm_ip in console config.json file"
-	sed -i -e "s@http:.*:@http://$vm_ip:@" \
-	  $POLICY_HOME/servers/console/webapps/ecomp/app/policyApp/Properties/config.json
-fi
