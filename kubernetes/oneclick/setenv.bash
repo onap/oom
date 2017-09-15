4aba389365e9cc136281d70f66701317656dc1f6
HuabingZhao <zhao.huabing@zte.com.cn>
2017-09-15 11:54:08 +0800

    Add msb and kube2msb registrator to oneclick
    Deploying MSB first and registrator last will ensure
    all the ONAP services can be registered to MSB.
    
    Issue-Id: OOM-278
    Change-Id: I3210f407c7b338b765117f8f2ba7a1d4920f3b32
    Signed-off-by: HuabingZhao <zhao.huabing@zte.com.cn>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-HELM_APPS=('mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc' 'dcae' 'log' 'cli')
+
+# Deploying MSB first and kube2msb last will ensure all the ONAP services can be registered to MSB
+HELM_APPS=('msb' 'mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc' 'dcae' 'log' 'cli' 'multicloud' 'kube2msb')
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
 ONAP_DOCKER_PASS=${ONAP_DOCKER_PASS:-docker}
