d6441bff9d2d92e10a688f53983ccd1be5716137
Mandeep Khinda <mandeep.khinda@amdocs.com>
2017-09-21 18:22:10 +0000

    dcaegen2 bootstrap container in K8s
    launching the dcaegen2 bootstrap container in OOM K8s.
    -based on dcaegen2/deployments/bootstrap/README-docker.md
    -requires an openstack private key pair file locally
    -requires mining and updating a yaml configuration file
     as per the comments in the sample file given.
    -unfortunately, my attempts to bundle the secret and configmap
     in the helm package didn't work out as they are external files
     so I create them using the createAll.bash script.
    
    Issue-ID: OOM-107
    
    Change-Id: I221f60af66ea0f7e97ddd8b92819dce9aa6b5a81
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

--- kubernetes/oneclick/setenv.bash
+++ kubernetes/oneclick/setenv.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Deploying MSB first and kube2msb last will ensure all the ONAP services can be registered to MSB
-HELM_APPS=('msb' 'mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc' 'dcae' 'log' 'cli' 'multicloud' 'clamp' 'kube2msb')
+HELM_APPS=('msb' 'mso' 'message-router' 'sdnc' 'vid' 'robot' 'portal' 'policy' 'appc' 'aai' 'sdc' 'dcaegen2' 'log' 'cli' 'multicloud' 'clamp' 'kube2msb')
 ONAP_DOCKER_REGISTRY=${ONAP_DOCKER_REGISTRY:-nexus3.onap.org:10001}
 ONAP_DOCKER_USER=${ONAP_DOCKER_USER:-docker}
 ONAP_DOCKER_PASS=${ONAP_DOCKER_PASS:-docker}
 ONAP_DOCKER_MAIL=${ONAP_DOCKER_MAIL:-$USERNAME@$USERDOMAIN}
+# Openstack key pair private key file location required to enable dcaegen2 installer CRUD operations in your Openstack
+# Ensure you set the name of your keypair in the dcae-parameters.yaml entry "keypair: "dcae-g2"
+# example: export OPENSTACK_PRIVATE_KEY_PATH=/home/user/Downloads/dcae-g2.pem
+OPENSTACK_PRIVATE_KEY_PATH=${OPENSTACK_PRIVATE_KEY_PATH:-~/.ssh/onap_rsa}
+# dcaegen2 bootstrap configuration input yaml file.  Start from the sample, and set your environments real values:
+# example: export DCAEGEN2_CONFIG_INPUT_FILE_PATH=/tmp/dcae-parameters.yaml
+DCAEGEN2_CONFIG_INPUT_FILE_PATH=${DCAEGEN2_CONFIG_INPUT_FILE_PATH:-../dcaegen2/dcae-parameters-sample.yaml}
