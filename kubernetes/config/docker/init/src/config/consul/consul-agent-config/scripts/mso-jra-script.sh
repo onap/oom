a9307ce25f2a24ce0871eeaea90cef0bd09cd6da
gfraboni <gino.fraboni@amdocs.com>
2017-09-22 11:30:59 -0400

    Add Consul health check support for MSO
    This push adds health checks for the MSO components.
    
    Issue-ID: OOM-86
    
    Change-Id: Ia8908e4225248f51c73d8d41aa8895cd6f9b511c
    Signed-off-by: gfraboni <gino.fraboni@amdocs.com>

---
+++ kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/mso-jra-script.sh
@@ -0,0 +1,18 @@
+## Query the health check API.
+HEALTH_CHECK_ENDPOINT="http://mso.onap-mso.svc.cluster.local:8080/networks/rest/healthcheck"
+HEALTH_CHECK_RESPONSE=$(curl -s $HEALTH_CHECK_ENDPOINT)
+
+READY=$(echo $HEALTH_CHECK_RESPONSE | grep "Application ready")
+
+if [ -n $READY ]; then
+  echo "Query against health check endpoint: $HEALTH_CHECK_ENDPOINT"
+  echo "Produces response: $HEALTH_CHECK_RESPONSE"
+  echo "Application is not in an available state"
+  return 2
+else
+  echo "Application is available."
+  return 0
+fi
+
+{"service": {"name": "mso-jra-healthcheck",
+  "check": {"script": "curl http://mso.onap-mso.svc.cluster.local:8080/networks/rest/healthcheck 2>&1 | grep 'Application ready' | wc -l| grep 1", "interval": "10s"}}}
