cc3c65097ad62d4cc4ebb0ed9a244f5d1f33c995
Mandeep Khinda <mandeep.khinda@amdocs.com>
2017-10-24 22:13:03 +0000

    synching up portal config again
    PORTAL-130 renamed various mounts and FQDNs
    
    Change-Id: Id1872422e7d876953ecb0a92ce55a8753d9d7eaa
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

--- kubernetes/config/docker/init/src/config/portal/mariadb/oom_updates.sql
+++ kubernetes/config/docker/init/src/config/portal/mariadb/oom_updates.sql
@@ -4,4 +4,4 @@ Any updates required by OOM to the portaldb are made here.
 1. split up SDC-FE and SDC-BE.  Originally both FE and BE point to the same IP
 while the OOM K8s version has these service split up.
 */
-UPDATE fn_app SET app_rest_endpoint = 'http://sdc.api.be.simpledemo.openecomp.org:8080/api/v2' where app_name = 'SDC';
+UPDATE fn_app SET app_rest_endpoint = 'http://sdc.api.be.simpledemo.onap.org:8080/api/v2' where app_name = 'SDC';
