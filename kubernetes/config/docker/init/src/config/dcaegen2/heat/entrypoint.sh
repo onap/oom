cd6fc842cd34cedd7645d737f62d1c543abfded1
Alexis de Talhouët <adetalhouet89@gmail.com>
2017-12-11 17:13:38 -0500

    Add support for DCAE in Amsterdam
    Change-Id: I64ec6faf7b45b293bf7110e9e23e31798bf9b841
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>
    Issue-ID: OOM-508

---
+++ kubernetes/config/docker/init/src/config/dcaegen2/heat/entrypoint.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+apt update -y
+apt -y install python-pip
+pip install python-openstackclient
+
+source /opt/heat/OOM-openrc.sh
+
+openstack stack create -t /opt/heat/onap_dcae.yaml -e /opt/heat/onap_dcae.env dcae
+
+# let the pod live even though we're done
+sleep infinity
