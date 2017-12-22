41463bcf3e6ddb2a8da4b3e1bc918db142a3feb8
mayankg2703 <mayank.gupta@amdocs.com>
2017-12-22 14:18:37 -0500

    mso mariadb health check
    Issue-ID: OOM-41
    
    Change-Id: Idcac63506a2d39a149d7cac6d269a5c48c15e37c
    Signed-off-by: mayankg2703 <mayank.gupta@amdocs.com>

---
+++ kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/mso-mariadb-script.sh
@@ -0,0 +1,14 @@
+NAME=$(/consul/config/bin/kubectl -n onap-mso get pod | grep -o "mariadb[^[:space:]]*")
+
+   if [ -n "$NAME" ]; then
+       if /consul/config/bin/kubectl -n onap-mso exec -it $NAME -- bash -c 'mysqladmin status -u root -p$MYSQL_ROOT_PASSWORD' > /dev/null; then
+         echo Success. mariadb process is running. 2>&1
+         exit 0
+      else
+         echo Failed. mariadb process is not running. 2>&1
+         exit 1
+      fi
+   else
+      echo Failed. mariadb container is offline. 2>&1
+      exit 1
+   fi
