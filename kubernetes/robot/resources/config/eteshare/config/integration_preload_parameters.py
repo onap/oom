0947cdf7e780641e1604ef3752483fa4d29efb3e
vaibhav_16dec <vaibhav.chopra@amdocs.com>
2018-01-17 15:40:59 +0000

    Robot config seggregation
    Issue-ID: OOM-560
    
    Change-Id: Iee9c7c78c6eb53b14b31d6e31f1539040daa88ba
    Signed-off-by: vaibhav_16dec <vaibhav.chopra@amdocs.com>

--- kubernetes/robot/resources/config/eteshare/config/integration_preload_parameters.py
+++ kubernetes/robot/resources/config/eteshare/config/integration_preload_parameters.py
@@ -5,15 +5,15 @@ GLOBAL_PRELOAD_PARAMETERS = {
         "pub_key" : "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAqqnA9BAiMLtjOPSYBfhzLu4CiBolWoskDg4KVwhTJVTTeB6CqrQNcadlGXxOHhCYuNCKkUmIVF4WTOisVOJ75Z1c4OMoZLL85xVPKSIeH63kgVugwgPYQu55NbbWX+rsbUha3LnElDhNviMM3iTPbD5nnhKixNERAJMTLKXvZZZGqxW94bREknYPQTT2qrk3YRqwldncopp6Nkgv3AnSJz2gc9tjxnWF0poTQnQm/3D6hiJICrzKfAV0EaPN0KdtYjPhKrYoy6Qb/tKOVaaqsvwfKBJGrT9LfcA7D7M/yj292RT1XN63hI84WC383LsaPJ6eWdDTE6zUP1eGTWCoOw== rsa-key-20161026",
         "repo_url_blob" : "https://nexus.onap.org/content/repositories/raw",
         "repo_url_artifacts" : "https://nexus.onap.org/content/groups/staging",
-        "demo_artifacts_version" : "DEMO_ARTIFACTS_VERSION_HERE",
-        "onap_private_net_id" : "OPENSTACK_NETWORK_ID_WITH_ONAP_ROUTE_HERE",
-        "onap_private_subnet_id" : "OPENSTACK_SUBNET_ID_WITH_ONAP_ROUTE_HERE",
-        "onap_private_net_cidr" : "NETWORK_CIDR_WITH_ONAP_ROUTE_HERE",
+        "demo_artifacts_version" : "{{ .Values.demoArtifactsVersion }}",
+        "onap_private_net_id" : "{{ .Values.openStackPrivateNetId }}",
+        "onap_private_subnet_id" : "{{ .Values.openStackPrivateSubnetId }}",
+        "onap_private_net_cidr" : "{{ .Values.openStackPrivateNetCidr }}",
         "dcae_collector_ip" : "10.0.4.102",
         "dcae_collector_port" : "8080",
-        "public_net_id" : "OPENSTACK_PUBLIC_NET_ID_HERE",
+        "public_net_id" : "{{ .Values.openStackPublicNetId }}",
         "cloud_env" : "${cloud_env}",
-   	    "install_script_version" : "${install_script_version}",
+            "install_script_version" : "${install_script_version}",
 ###
 # vims_preload  same for every instantiation
 ###
@@ -24,28 +24,28 @@ GLOBAL_PRELOAD_PARAMETERS = {
         "ralf_image_name" : "${vm_image_name}",
         "ellis_image_name" : "${vm_image_name}",
         "dns_image_name" : "${vm_image_name}",
-	    "bono_flavor_name" : "${vm_flavor_name}",
-	    "sprout_flavor_name" : "${vm_flavor_name}",
-	    "homer_flavor_name" : "${vm_flavor_name}",
-	    "homestead_flavor_name" : "${vm_flavor_name}",
-	    "ralf_flavor_name" : "${vm_flavor_name}",
-	    "ellis_flavor_name" : "${vm_flavor_name}",
-	    "dns_flavor_name" : "${vm_flavor_name}",
-	    "repo_url" : "http://repo.cw-ngv.com/stable",
-	    "zone" : "me.cw-ngv.com",
-	    "dn_range_start" : "2425550000",
-	    "dn_range_length" : "10000",
-	    "dnssec_key" : "9FPdYTWhk5+LbhrqtTPQKw==",
+            "bono_flavor_name" : "${vm_flavor_name}",
+            "sprout_flavor_name" : "${vm_flavor_name}",
+            "homer_flavor_name" : "${vm_flavor_name}",
+            "homestead_flavor_name" : "${vm_flavor_name}",
+            "ralf_flavor_name" : "${vm_flavor_name}",
+            "ellis_flavor_name" : "${vm_flavor_name}",
+            "dns_flavor_name" : "${vm_flavor_name}",
+            "repo_url" : "http://repo.cw-ngv.com/stable",
+            "zone" : "me.cw-ngv.com",
+            "dn_range_start" : "2425550000",
+            "dn_range_length" : "10000",
+            "dnssec_key" : "9FPdYTWhk5+LbhrqtTPQKw==",
 ###
 # vlb_preload same for every instantiation
 ###
-		"vlb_image_name" : "${vm_image_name}",
-		"vlb_flavor_name" : "${vm_flavor_name}",
+                "vlb_image_name" : "${vm_image_name}",
+                "vlb_flavor_name" : "${vm_flavor_name}",
 ###
 # vlb_preload same for every instantiation
 ###
-		"vfw_image_name" : "${vm_image_name}",
-		"vfw_flavor_name" : "${vm_flavor_name}",
+                "vfw_image_name" : "${vm_image_name}",
+                "vfw_flavor_name" : "${vm_flavor_name}",
 ###
     },
 
@@ -60,11 +60,11 @@ GLOBAL_PRELOAD_PARAMETERS = {
             "protected_private_net_cidr" : "192.168.20.0/24",
             "vfw_private_ip_0" : "192.168.10.100",
             "vfw_private_ip_1" : "192.168.20.100",
-            "vfw_private_ip_2" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.1",
+            "vfw_private_ip_2" : "{{ .Values.openStackOamNetworkCidrPrefix }}.1",
             "vpg_private_ip_0" : "192.168.10.200",
-            "vpg_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.2",
+            "vpg_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.2",
             "vsn_private_ip_0" : "192.168.20.250",
-            "vsn_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.3",
+            "vsn_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.3",
             'vfw_name_0':'vofwl01fwl${hostid}',
             'vpg_name_0':'vofwl01pgn${hostid}',
             'vsn_name_0':'vofwl01snk${hostid}'
@@ -73,41 +73,41 @@ GLOBAL_PRELOAD_PARAMETERS = {
             "vlb_private_net_id" : "volb01_private${hostid}",
             "vlb_private_net_cidr" : "192.168.30.0/24",
             "vlb_private_ip_0" : "192.168.30.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.4",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.4",
             "vdns_private_ip_0" : "192.168.30.110",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.5",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.5",
             'vlb_name_0':'vovlblb${hostid}',
             'vdns_name_0':'vovlbdns${hostid}',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24",
-  			"pktgen_private_net_cidr" : "192.168.9.0/24"
-    	    
+            "vlb_private_net_cidr" : "192.168.10.0/24",
+                        "pktgen_private_net_cidr" : "192.168.9.0/24"
+
         },
         "dnsscaling_preload.template" : {
             "vlb_private_net_id" : "volb01_private${hostid}",
             "vlb_private_ip_0" : "192.168.30.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.4",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.4",
             "vdns_private_ip_0" : "192.168.30.222",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.6",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.6",
             'scaling_vdns_name_0':'vovlbscaling${hostid}',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24"
+            "vlb_private_net_cidr" : "192.168.10.0/24"
         },
         "vvg_preload.template" : {
         }
     },
 # heat template parameter values for heat template instances created during Closed-Loop test cases
     "Closed-Loop" : {
-		"vfw_preload.template": {
+                "vfw_preload.template": {
             "unprotected_private_net_id" : "clfwl01_unprotected${hostid}",
             "unprotected_private_net_cidr" : "192.168.110.0/24",
             "protected_private_net_id" : "clfwl01_protected${hostid}",
             "protected_private_net_cidr" : "192.168.120.0/24",
             "vfw_private_ip_0" : "192.168.110.100",
             "vfw_private_ip_1" : "192.168.120.100",
-            "vfw_private_ip_2" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.11",
+            "vfw_private_ip_2" : "{{ .Values.openStackOamNetworkCidrPrefix }}.11",
             "vpg_private_ip_0" : "192.168.110.200",
-            "vpg_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.12",
+            "vpg_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.12",
             "vsn_private_ip_0" : "192.168.120.250",
-            "vsn_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.13",
+            "vsn_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.13",
             'vfw_name_0':'clfwl01fwl${hostid}',
             'vpg_name_0':'clfwl01pgn${hostid}',
             'vsn_name_0':'clfwl01snk${hostid}'
@@ -116,22 +116,22 @@ GLOBAL_PRELOAD_PARAMETERS = {
             "vlb_private_net_id" : "cllb01_private${hostid}",
             "vlb_private_net_cidr" : "192.168.130.0/24",
             "vlb_private_ip_0" : "192.168.130.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.14",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.14",
             "vdns_private_ip_0" : "192.168.130.110",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.15",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.15",
             'vlb_name_0':'clvlblb${hostid}',
             'vdns_name_0':'clvlbdns${hostid}',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24",
-  			"pktgen_private_net_cidr" : "192.168.9.0/24"
+            "vlb_private_net_cidr" : "192.168.10.0/24",
+                        "pktgen_private_net_cidr" : "192.168.9.0/24"
         },
         "dnsscaling_preload.template" : {
             "vlb_private_net_id" : "cllb01_private${hostid}",
             "vlb_private_ip_0" : "192.168.130.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.14",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.14",
             "vdns_private_ip_0" : "192.168.130.222",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.16",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.16",
             'scaling_vdns_name_0':'clvlbscaling${hostid}',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24"
+            "vlb_private_net_cidr" : "192.168.10.0/24"
         },
         "vvg_preload.template" : {
         }
@@ -145,11 +145,11 @@ GLOBAL_PRELOAD_PARAMETERS = {
             "protected_private_net_cidr" : "192.168.120.0/24",
             "vfw_private_ip_0" : "192.168.110.100",
             "vfw_private_ip_1" : "192.168.120.100",
-            "vfw_private_ip_2" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.11",
+            "vfw_private_ip_2" : "{{ .Values.openStackOamNetworkCidrPrefix }}.11",
             "vpg_private_ip_0" : "192.168.110.200",
-            "vpg_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.12",
+            "vpg_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.12",
             "vsn_private_ip_0" : "192.168.120.250",
-            "vsn_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.13",
+            "vsn_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.13",
             'vfw_name_0':'demofwl01fwl',
             'vpg_name_0':'demofwl01pgn',
             'vsn_name_0':'demofwl01snk'
@@ -158,25 +158,24 @@ GLOBAL_PRELOAD_PARAMETERS = {
             "vlb_private_net_id" : "demolb_private",
             "vlb_private_net_cidr" : "192.168.130.0/24",
             "vlb_private_ip_0" : "192.168.130.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.14",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.14",
             "vdns_private_ip_0" : "192.168.130.110",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.15",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.15",
             'vlb_name_0':'demovlblb',
             'vdns_name_0':'demovlbdns',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24",
-  			"pktgen_private_net_cidr" : "192.168.9.0/24"
+            "vlb_private_net_cidr" : "192.168.10.0/24",
+                        "pktgen_private_net_cidr" : "192.168.9.0/24"
         },
         "dnsscaling_preload.template" : {
             "vlb_private_net_id" : "demolb_private",
             "vlb_private_ip_0" : "192.168.130.100",
-            "vlb_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.14",
+            "vlb_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.14",
             "vdns_private_ip_0" : "192.168.130.222",
-            "vdns_private_ip_1" : "OPENSTACK_OAM_NETWORK_CIDR_PREFIX_HERE.16",
+            "vdns_private_ip_1" : "{{ .Values.openStackOamNetworkCidrPrefix }}.16",
             'scaling_vdns_name_0':'demovlbscaling',
-    	    "vlb_private_net_cidr" : "192.168.10.0/24"
+            "vlb_private_net_cidr" : "192.168.10.0/24"
         },
         "vvg_preload.template" : {
         }
     }
 }
-
