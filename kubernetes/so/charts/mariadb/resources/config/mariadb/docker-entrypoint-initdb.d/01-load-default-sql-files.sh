b743e5a265c489437fef3f83c2c70a54e495af73
Mike Elliott <mike.elliott@amdocs.com>
2018-02-12 15:54:03 -0500

    Add standardized helm chart for so
    ** Do NOT merge! **
    This is a WIP and is being pushed as a draft to provide
    visibility, and to solicit feedback, towards a standardization
    of helm charts for the ONAP platform.
    
    This is a standardization (based on helm community best practices)
    of a Helm chart for the Service Orchestrator (so) in ONAP.
    
    How to deploy the so chart (outside of the parent onap chart) from
    the local oom/kubernetes codebase.
    
    ** need to create/update dependencies defined in the chart's
    ** requirements.yaml
    helm dep update so/
    
    ** deploy the so helm chart with the "release" name of 'onap'
    helm install so/ -n onap
    
    Change-Id: I22471eb9fe0dec32941e14dc63857222c80ebe20
    Issue-ID: OOM-265
    Signed-off-by: Mike Elliott <mike.elliott@amdocs.com>

---
+++ kubernetes/so/charts/mariadb/resources/config/mariadb/docker-entrypoint-initdb.d/01-load-default-sql-files.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# ============LICENSE_START==========================================
+# ===================================================================
+# Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+# ===================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END============================================
+#
+# ECOMP and OpenECOMP are trademarks
+# and service marks of AT&T Intellectual Property.
+#
+#
+cd /docker-entrypoint-initdb.d/db-sql-scripts/bulkload-files/demo-vfw
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -f < create_mso_db-demo-vfw.sql
---
+++ kubernetes/so/resources/config/docker-files/scripts/start-jboss-server.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# Copyright 2015 AT&T Intellectual Properties
+##############################################################################
+#       Script to initialize the chef-repo branch and.chef
+#
+##############################################################################
+# Copy the certificates
+echo 'Copying the *.crt provided in /shared folder'
+cp --verbose /shared/*.crt /usr/local/share/ca-certificates
+update-ca-certificates
+
+echo 'Running in JBOSS'
+su - jboss
+
+DEBUG=''
+if [ "$JBOSS_DEBUG" = true ] ; then
+  DEBUG="--debug"
+fi
+
+#Start the chef-solo if mso-docker.json contains some data.
+if [ -s /var/berks-cookbooks/${CHEF_REPO_NAME}/environments/mso-docker.json ]
+then
+	echo "mso-docker.json has some configuration, replay the recipes."
+	chef-solo -c /var/berks-cookbooks/${CHEF_REPO_NAME}/solo.rb -o recipe[mso-config::apih],recipe[mso-config::bpmn],recipe[mso-config::jra]
+else
+	echo "mso-docker.json is empty, do not replay the recipes."
+fi
+
+JBOSS_PIDFILE=/tmp/jboss-standalone.pid
+$JBOSS_HOME/bin/standalone.sh ${DEBUG} -c standalone-full-ha-mso.xml &
+JBOSS_PID=$!
+# Trap common signals and relay them to the jboss process
+trap "kill -HUP  $JBOSS_PID" HUP
+trap "kill -TERM $JBOSS_PID" INT
+trap "kill -QUIT $JBOSS_PID" QUIT
+trap "kill -PIPE $JBOSS_PID" PIPE
+trap "kill -TERM $JBOSS_PID" TERM
+if [ "x$JBOSS_PIDFILE" != "x" ]; then
+  echo $JBOSS_PID > $JBOSS_PIDFILE
+fi
+# Wait until the background process exits
+WAIT_STATUS=128
+while [ "$WAIT_STATUS" -ge 128 ]; do
+   wait $JBOSS_PID 2>/dev/null
+   WAIT_STATUS=$?
+   if [ "$WAIT_STATUS" -gt 128 ]; then
+      SIGNAL=`expr $WAIT_STATUS - 128`
+      SIGNAL_NAME=`kill -l $SIGNAL`
+      echo "*** JBossAS process ($JBOSS_PID) received $SIGNAL_NAME signal ***" >&2
+   fi
+done
+if [ "$WAIT_STATUS" -lt 127 ]; then
+   JBOSS_STATUS=$WAIT_STATUS
+else
+   JBOSS_STATUS=0
+fi
+if [ "$JBOSS_STATUS" -ne 10 ]; then
+      # Wait for a complete shudown
+      wait $JBOSS_PID 2>/dev/null
+fi
+if [ "x$JBOSS_PIDFILE" != "x" ]; then
+      grep "$JBOSS_PID" $JBOSS_PIDFILE && rm $JBOSS_PIDFILE
+fi
