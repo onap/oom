7f3df15bfa95ab398fe54d4906a4209634a51e26
pramod <pramod.kumarsharma@amdocs.com>
2018-02-20 09:38:28 +0000

    Apache 2 License updation for Consul
    Issue-ID: OOM-708
    Change-Id: I95153721f6b857621c413b884959ebeab5a02ad7
    Signed-off-by: pramod <pramod.kumarsharma@amdocs.com>

--- kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/mr-kafka-health.sh
+++ kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/mr-kafka-health.sh
@@ -1,3 +1,19 @@
+# Copyright © 2017 Amdocs
+# Copyright © 2017 Bell Canada
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 kafkapod=$(/consul/config/bin/kubectl -n onap-message-router get pod | grep -o "global-kafka-[^[:space:]]*")
 if [ -n "$kafkapod" ]; then
    if /consul/config/bin/kubectl -n onap-message-router exec -it $kafkapod -- ps ef | grep -i kafka; then
