eda78c46196053d290523d28f11b3a5409a7bf63
atulan <atul.angrish@amdocs.com>
2018-02-26 11:44:23 +0000

    Removing aai-config
    Issue-ID: OOM-764
    Change-Id: If1e860a310dfe42df23b3ac754961e0f070e1ed2
    Signed-off-by: atulan <atul.angrish@amdocs.com>

--- kubernetes/config/docker/init/src/config/aai/aai-config/cookbooks/aai-resources/aai-resources-config/attributes/titan-realtime.rb
+++
@@ -1,3 +0,0 @@
-node.default["aai-resources-config"]["STORAGE_HOSTNAME"] = 'localhost'
-node.default["aai-resources-config"]["STORAGE_HBASE_TABLE"] = 'aaigraph.dev'
-node.default["aai-resources-config"]["STORAGE_HBASE_ZOOKEEPER_ZNODE_PARENT"] = '/hbase'
