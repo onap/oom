e86ea9c756b1df294aae2121bc1c2255fa026a8a
atulan <atul.angrish@amdocs.com>
2018-02-26 11:44:23 +0000

    Removing aai-config Issue-ID: OOM-764
    Change-Id: If1e860a310dfe42df23b3ac754961e0f070e1ed2
    Signed-off-by: atulan <atul.angrish@amdocs.com>

--- kubernetes/config/docker/init/src/config/aai/aai-config/cookbooks/aai-traversal/aai-traversal-config/attributes/titan-realtime.rb
+++
@@ -1,3 +0,0 @@
-node.default["aai-traversal-config"]["STORAGE_HOSTNAME"] = 'localhost'
-node.default["aai-traversal-config"]["STORAGE_HBASE_TABLE"] = 'aaigraph.dev'
-node.default["aai-traversal-config"]["STORAGE_HBASE_ZOOKEEPER_ZNODE_PARENT"] = '/hbase'
