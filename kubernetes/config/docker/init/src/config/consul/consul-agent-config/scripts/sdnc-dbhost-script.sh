5e4c42b3450242cf4b49f85c9d6658de3339f8d2
ZZZ <XXX.YYY@amdocs.com>
2018-02-27 15:04:26 +0000

    Run all components in one namespace
    Change-Id: I5fcd4d577c1fda4de27842807c7cf7a5d372756e
    Issue-ID: OOM-722
    Signed-off-by: BorislavG <Borislav.Glozman@amdocs.com>

--- kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/sdnc-dbhost-script.sh
+++ kubernetes/config/docker/init/src/config/consul/consul-agent-config/scripts/sdnc-dbhost-script.sh
@@ -1,6 +1,6 @@
-SDNC_DBHOST_POD=$(/consul/config/bin/kubectl -n onap-sdnc  get pod | grep -o "sdnc-dbhost-[^[:space:]]*")
+SDNC_DBHOST_POD=$(/consul/config/bin/kubectl -n onap-namespace  get pod | grep -o "sdnc-dbhost-[^[:space:]]*")
 if [ -n "$SDNC_DBHOST_POD" ]; then
-   if /consul/config/bin/kubectl -n onap-sdnc exec -it $SDNC_DBHOST_POD -- ./healthcheck.sh |grep -i "mysqld is alive"; then
+   if /consul/config/bin/kubectl -n onap-namespace exec -it $SDNC_DBHOST_POD -- ./healthcheck.sh |grep -i "mysqld is alive"; then
       echo Success. SDNC DBHost is running. 2>&1
       exit 0
    else
