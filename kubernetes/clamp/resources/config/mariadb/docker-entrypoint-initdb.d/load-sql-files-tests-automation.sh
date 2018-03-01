13e8b0f466db1e454c423c25fd47277ad48013bc
atulan <atul.angrish@amdocs.com>
2018-03-01 18:49:13 +0000

    Apache 2 License updation for clamp
    Change-Id: I21f49a3310787c6e2acaa72462027769aac1886f
    Issue-ID: OOM-677
    Signed-off-by: atulan <atul.angrish@amdocs.com>

--- kubernetes/clamp/resources/config/mariadb/docker-entrypoint-initdb.d/load-sql-files-tests-automation.sh
+++ kubernetes/clamp/resources/config/mariadb/docker-entrypoint-initdb.d/load-sql-files-tests-automation.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 ###
-# ============LICENSE_START=======================================================
 # ONAP CLAMP
-# ================================================================================
-# Copyright (C) 2017 AT&T Intellectual Property. All rights
-#                             reserved.
-# ================================================================================
+# Copyright 2017 AT&T Intellectual Property
+# Copyright 2017 Amdocs, Bell Canada
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -18,8 +16,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ============LICENSE_END============================================
-# ===================================================================
 # ECOMP is a trademark and service mark of AT&T Intellectual Property.
 ###
 
