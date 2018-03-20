012249adf029d669386b460466bbb9beb70826d6
mayankg2703 <mayank.gupta@amdocs.com>
2018-03-20 05:42:53 +0000

    Add Standardized Configuration to POLICY
    Change-Id: Iabdb2bba14b5095c08ff2920aec97a213e0acb30
    Issue-ID: OOM-745
    Signed-off-by: mayankg2703 <mayank.gupta@amdocs.com>

--- kubernetes/policy/charts/mariadb/resources/config/db.sh
+++ kubernetes/policy/charts/mariadb/resources/config/db.sh
@@ -1,4 +1,4 @@
-# Copyright © 2017 Amdocs, Bell Canada
+# Copyright © 2017 Amdocs, Bell Canada, AT&T
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-apiVersion: v1
-description: Policy access point
-name: policy
-version: 2.0.0
+#!/bin/bash -xv
+
+for db in support onap_sdk log
+do
+	mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --execute "CREATE DATABASE IF NOT EXISTS ${db};"
+	mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --execute "GRANT ALL PRIVILEGES ON \`${db}\`.* TO '${MYSQL_USER}'@'%' ;"
+done
+
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --execute "FLUSH PRIVILEGES;"
