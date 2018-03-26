69303562407935c01facb831c85134f77b3579ae
vaibhav_16dec <vaibhav.chopra@amdocs.com>
2018-03-26 10:01:46 +0000

    Apache2 license addition for sql and js files
    Issue-ID: OOM-825
    
    Change-Id: I0cd8748adc7aa1a66cefd4a2b7d660d671a550b3
    Signed-off-by: vaibhav_16dec <vaibhav.chopra@amdocs.com>

--- kubernetes/vid/resources/config/lf_config/vid-pre-init.sql
+++ kubernetes/vid/resources/config/lf_config/vid-pre-init.sql
@@ -1,22 +1,18 @@
-/*-
- * ============LICENSE_START=======================================================
- * VID
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ============LICENSE_END=========================================================
- */
+/* Copyright © 2017 AT&T, Amdocs, Bell Canada
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 CREATE TABLE IF NOT EXISTS `vid_openecomp_epsdk`.`schema_info` (
    `SCHEMA_ID` VARCHAR(25) NOT NULL,
    `SCHEMA_DESC` VARCHAR(75) NOT NULL,
