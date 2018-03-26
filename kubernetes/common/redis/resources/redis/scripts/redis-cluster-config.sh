aa0c3d651b88689a62f32b38da2a0332abc013cb
BorislavG <Borislav.Glozman@amdocs.com>
2018-03-26 21:13:34 +0300

    Add chart for redis cluster
    Change-Id: I5087c5180d5489d56fa1eea8ad8ac50f905bb4ba
    Issue-ID: DCAEGEN2-417
    Signed-off-by: BorislavG <Borislav.Glozman@amdocs.com>
    Signed-off-by: Lusheng Ji <lji@research.att.com>

---
+++ kubernetes/common/redis/resources/redis/scripts/redis-cluster-config.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# ================================================================================
+# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+
+if [[ "$HOSTNAME" == *redis-cluster-0 ]]; then
+
+  NODES=""
+  echo "====> wait for all {{.Values.replicaCount}} redis pods up"
+  while [ "$(echo $NODES | wc -w)" -lt {{.Values.replicaCount}} ]
+  do
+    echo "======> $(echo $NODES |wc -w) / {{.Values.replicaCount}} pods up"
+    sleep 5
+    RESP=$(wget -vO- --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  --header "Authorization: Bearer $(</var/run/secrets/kubernetes.io/serviceaccount/token)" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods?labelSelector=name={{ include "common.name" . }})
+    NODES=$(echo $RESP | jq -r '.items[].status.podIP + ":{{.Values.service.externalPort}}"')
+  done
+  echo "====> all {{.Values.replicaCount}} redis cluster pods are up. wait 10 seconds before the next step"; echo
+  sleep 10
+
+  echo "====> Configure the cluster"
+
+  # $NODES w/o quotes
+  redis-trib create --replicas 1 $NODES
+
+fi
