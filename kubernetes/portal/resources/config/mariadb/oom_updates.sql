3cd02709a99f35aa540194eefe5212df0dcb3d4a
Mandeep Khinda <mandeep.khinda@amdocs.com>
2018-03-27 01:26:59 +0000

    Standardizing ONAP Portal
    -synched up with Beijing (master)
    -pods come up without cassandra music:
    sed: couldn't edit /docker-entrypoint-initdb.d/\
    zzz_portal.cql: not a regular file
    
    ONAPPORTAL webapp in tomcat is failing to star
    resulting in HTTP 404 robot health
    
    Not sure how logging works in beijing so removed
    some of the ubuntu-init dir creation.  need someone
    familiar with logging to investigate
    
    Issue-ID: OOM-746
    
    Change-Id: I123d1ec6889db40b1721475a43665ef8efefc057
    Signed-off-by: Mandeep Khinda <mandeep.khinda@amdocs.com>

--- kubernetes/portal/resources/config/mariadb/oom_updates.sql
+++
@@ -1,8 +0,0 @@
-USE portal;
-/*
-Any updates required by OOM to the portaldb are made here.
-1. split up SDC-FE and SDC-BE.  Originally both FE and BE point to the same IP
-while the OOM K8s version has these service split up.
-*/
-UPDATE fn_app SET app_rest_endpoint = 'http://sdc.api.be.simpledemo.onap.org:8080/api/v2' where app_name = 'SDC';
-UPDATE fn_app SET app_url = 'http://cli.api.simpledemo.onap.org:8080', app_type = 1 where app_name='CLI';
