c0a7f1506a4cf372d5760cf7cd13d6ee2e4fd334
vaibhav_16dec <vaibhav.chopra@amdocs.com>
2018-03-27 10:01:31 +0000

    Apache 2 license addition for all configuration
    Issue-ID: OOM-833
    
    Change-Id: I3312246ae5d9f1003cf0658d5f37d918f20f28f9
    Signed-off-by: vaibhav_16dec <vaibhav.chopra@amdocs.com>

--- kubernetes/robot/resources/config/eteshare/config/integration_robot_properties.py
+++ kubernetes/robot/resources/config/eteshare/config/integration_robot_properties.py
@@ -1,3 +1,17 @@
+# Copyright © 2017 Amdocs, Bell Canada
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # aaf info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_AAF_SERVER = "http://10.0.12.1:8101"
 GLOBAL_AAF_USERNAME = "dgl@openecomp.org"
