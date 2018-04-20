d25d50671945c876a3e2b2e1058b748c5d3d3531
Hong Guan <hg4105@att.com>
2018-04-20 13:49:23 -0400

    Enhanced OOM TOSCA solution Issue-ID: OOM-979
    Change-Id: I946013a73e85434292880790e487883f3be63493
    Signed-off-by: Hong Guan <hg4105@att.com>

--- TOSCA/Helm/helminstall.sh
+++ TOSCA/Helm/helminstall.sh
@@ -19,12 +19,5 @@
 printf "%s" "$*"
 printf `pwd`
 printf "%s" "---------------"
-kubectl create namespace $1-$2
-kubectl create clusterrolebinding $1-$2-admin-binding --clusterrole=cluster-admin --serviceaccount=$1-$2:default
-# assign default auth token
-if [[ -z $ONAP_DEFAULT_AUTH_TOKEN ]]; then
-  DEFAULT_SECRET=`kubectl get secrets -n $1-$2 | grep default-token |  awk '{ print $1}'`
-  ONAP_DEFAULT_AUTH_TOKEN=`kubectl get secrets $DEFAULT_SECRET -n $1-$2 -o yaml | grep  'token:'  | awk '{ print $2}' | base64 -d`
-fi
-kubectl --namespace $1-$2 create secret docker-registry $1-docker-registry-key --docker-server=nexus3.onap.org:10001 --docker-username=docker --docker-password=docker --docker-email=@
-helm install ../$2/ --name $1-$2 --namespace $1 --set nsPrefix=$1,nodePortPrefix=302,kubeMasterAuthToken=$ONAP_DEFAULT_AUTH_TOKEN
+
+helm install local/$2 --name $2 --namespace $1
