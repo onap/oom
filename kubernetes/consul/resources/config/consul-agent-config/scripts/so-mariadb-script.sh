83e5bdd5b2c733e219c53afb1b23eceee1243e31
Priyanka Jain <Priyanka.Jain3@amdocs.com>
2018-05-09 07:15:49 +0000

    Config Changes in Consul
    Issue-ID: OOM-1031
    
    Change-Id: Ic67cdafce7f070bb5a7c9958ca722fda6d77b32d
    Signed-off-by: Priyanka Jain <Priyanka.Jain3@amdocs.com>

--- kubernetes/consul/resources/config/consul-agent-config/scripts/so-mariadb-script.sh
+++ kubernetes/consul/resources/config/consul-agent-config/scripts/so-mariadb-script.sh
@@ -1,4 +1,4 @@
-NAME=$(/consul/bin/kubectl -n {{ include "common.namespace" . }} get pod | grep -o "so-db[^[:space:]]*")
+NAME=$(/consul/bin/kubectl -n {{ include "common.namespace" . }} get pod | grep -o "[^[:space:]]*-so-db[^[:space:]]*")
 
    if [ -n "$NAME" ]; then
        if /consul/bin/kubectl -n {{ include "common.namespace" . }} exec -it $NAME -- bash -c 'mysqladmin status -u root -p$MYSQL_ROOT_PASSWORD' > /dev/null; then
