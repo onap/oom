dc5bf8b5e5fdadf7e8d42e5edc6bc67b0b025350
Priyanka Jain <Priyanka.Jain3@amdocs.com>
2018-05-09 08:42:50 +0000

    Consul Stateful Set
    Issue-ID: OOM-1032
    
    Change-Id: I911085dffc35067d844b5dcb286581fa0897a27a
    Signed-off-by: Priyanka Jain <Priyanka.Jain3@amdocs.com>

--- kubernetes/consul/resources/config/consul-agent-config/scripts/tabular-db-availability.sh
+++ kubernetes/consul/resources/config/consul-agent-config/scripts/tabular-db-availability.sh
@@ -1,13 +1,13 @@
 
 # Query the Hbase service for the cluster status.
-GET_CLUSTER_STATUS_RESPONSE=$(curl -si -X GET -H "Accept: text/xml" http://hbase:8080/status/cluster)
+GET_CLUSTER_STATUS_RESPONSE=$(curl -si -X GET -H "Accept: text/xml" http://aai-hbase:8080/status/cluster)
 
 if [ -z "$GET_CLUSTER_STATUS_RESPONSE" ]; then
   echo "Tabular store is unreachable."
-  return 2 
+  return 2
 fi
 
-# Check the resulting status JSON to see if there is a 'DeadNodes' stanza with 
+# Check the resulting status JSON to see if there is a 'DeadNodes' stanza with
 # entries.
 DEAD_NODES=$(echo $GET_CLUSTER_STATUS_RESPONSE | grep "<DeadNodes/>")
 
