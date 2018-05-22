9eb4adc754e6f8da4cc7ed2f2facebf544593cb3
Priyanka <Priyanka.Jain3@amdocs.com>
2018-05-22 10:53:36 +0000

    Consul Health Check changes for MessageRouter
    Issue-ID: OOM-987
    
    Change-Id: If55855dbadcb009da3e82a2a3aa05bd49915a517
    Signed-off-by: Priyanka <Priyanka.Jain3@amdocs.com>

--- kubernetes/consul/resources/config/consul-agent-config/scripts/mr-zookeeper-health.sh
+++ kubernetes/consul/resources/config/consul-agent-config/scripts/mr-zookeeper-health.sh
@@ -1,6 +1,6 @@
-zkpod=$(/consul/bin/kubectl -n {{ include "common.namespace" . }} get pod | grep -o "message-router-zookeeper-[^[:space:]]*")
+zkpod=$(/consul/bin/kubectl -n {{ include "common.namespace" . }} get pod | grep -o "[^[:space:]]*-message-router-zookeeper-[^[:space:]]*")
 if [ -n "$zkpod" ]; then
-   if /consul/bin/kubectl -n {{ include "common.namespace" . }} exec -it $zkpod -- ps ef | grep -i zookeeper; then
+   if /consul/bin/kubectl -n {{ include "common.namespace" . }} exec -it $zkpod -- ps aux | grep -i zookeeper; then
       echo Success. Zookeeper process is running. 2>&1
       exit 0
    else
