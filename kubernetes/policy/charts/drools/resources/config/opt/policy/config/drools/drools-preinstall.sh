129a39a963b194350a9e871769477ea5bb2e5e9c
Durgpal <durgpal@amdocs.com>
2018-08-03 07:28:36 +0000

    License Addition for Dcaegen2,Policy,Dmaap
    Issue-ID: OOM-1305
    
    Change-Id: Idb6ad7004b7a4dc20ffafda72d60c8547eedfb9e
    Signed-off-by: Durgpal <durgpal@amdocs.com>

--- kubernetes/policy/charts/drools/resources/config/opt/policy/config/drools/drools-preinstall.sh
+++ kubernetes/policy/charts/drools/resources/config/opt/policy/config/drools/drools-preinstall.sh
@@ -3,7 +3,7 @@
 # ============LICENSE_START=======================================================
 # ONAP
 # ================================================================================
-# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# Copyright 2018 AT&T, Amdocs, Bell Canada Intellectual Property. All rights reserved
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
