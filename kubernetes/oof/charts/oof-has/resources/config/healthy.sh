02725a96c67ef53843627bb662fa3405421b22d9
Nishukumar <nishu.kumar@amdocs.com>
2018-08-03 09:17:23 +0000

    License addition for oof,multicloud,esr
    Issue-ID: OOM-1307
    
    Change-Id: I77cd304ae84fd5a158511a5cfff6ece78c45b4e1
    Signed-off-by: Nishukumar <nishu.kumar@amdocs.com>

--- kubernetes/oof/charts/oof-has/resources/config/healthy.sh
+++ kubernetes/oof/charts/oof-has/resources/config/healthy.sh
@@ -1,4 +1,5 @@
 # Copyright © 2017 Amdocs, Bell Canada
+# Modifications Copyright (C) 2018 AT&T,VMware
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
