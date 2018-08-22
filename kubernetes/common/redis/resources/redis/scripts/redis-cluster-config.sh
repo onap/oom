ec2eae4793f73843712d23fdd09faa2074dcfff9
pramod <pramod.kumarsharma@amdocs.com>
2018-08-22 02:22:31 +0000

    Migrate DCAE Redis Helm charts to OOM/common
    Change-Id: I4e0eab3c4c7467d49af86a9efa37a17034e127eb
    Issue-ID: OOM-1180
    Signed-off-by: pramod <pramod.kumarsharma@amdocs.com>

---
+++ kubernetes/common/redis/resources/redis/scripts/redis-cluster-config.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ================================================================================
+# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+(if [[ "$HOSTNAME" == *{{.Chart.Name}}-0 ]]; then
+  echo "delay by 10 seconds for redis server starting"
+  sleep 10
+
+  NODES=""
+  echo "====> wait for all {{.Values.replicaCount}} redis pods up"
+  while [ "$(echo $NODES | wc -w)" -lt {{.Values.replicaCount}} ]
+  do
+    echo "======> $(echo $NODES |wc -w) / {{.Values.replicaCount}} pods up"
+    sleep 5
+    RESP=$(wget -vO- --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  --header "Authorization: Bearer $(</var/run/secrets/kubernetes.io/serviceaccount/token)" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/{{.Release.Namespace}}/pods?labelSelector=app={{.Chart.Name}})
+
+    IPS=$(echo $RESP | jq -r '.items[].status.podIP')
+    IPS2=$(echo $IPS | sed -e 's/[a-zA-Z]*//g')
+    echo "======> IPs: ["$IPS2"]"
+    NODES=""
+    for I in $IPS2; do NODES="$NODES $I:{{.Values.service.externalPort}}"; done
+    echo "======> nodes: ["$NODES"]"
+  done
+  echo "====> all {{.Values.replicaCount}} redis cluster pods are up. wait 50 seconds before the next step"; echo
+  sleep 50
+
+  echo "====> Configure the cluster"
+
+  # $NODES w/o quotes
+  echo "======> nodes: [$(echo $NODES |paste -s)]"
+  redis-trib create --replicas 1 $(echo $NODES |paste -s)
+fi ) &
+
+redis-server /conf/redis.conf
+
