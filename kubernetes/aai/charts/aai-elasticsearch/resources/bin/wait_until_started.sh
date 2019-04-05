e46728ac73ff4272421bf8640169334ebb5a5d29
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-04-05 14:09:57 -0700

    AAI HELM code transfer
    Remove kubernetes/aai code from OOM.
    Add oom/aai as submodule of OOM in kubernetes/aai
    location.
    
    Change-Id: I7641a0cd4b0ecef174569c9450b09f2f7c211f44
    Issue-ID: CIMAN-250
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- kubernetes/aai/charts/aai-elasticsearch/resources/bin/wait_until_started.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-RET=1
-
-while [[ RET -ne 0 ]]; do
-    echo "Waiting for Elasticsearch to become ready before running sgadmin..."
-    curl -XGET -k "https://localhost:{{ .Values.service.internalPort }}/" >/dev/null 2>&1
-    RET=$?
-    sleep 5
-done
