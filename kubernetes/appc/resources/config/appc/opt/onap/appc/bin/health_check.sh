d0d6919071303f14e6cc9f2f642fb588a12e000c
Taka Cho <takamune.cho@att.com>
2019-04-23 18:00:46 -0400

    remove startODL string check
    it is not necessary to check
    startODL is runnning or no.
    in the past, startODL took 30-40 mins
    From dublin, startODL only takes less than 30 sec
    
    Change-Id: I5b5d80a1bde2d0d292d809dd202e1c3a15a1cf4d
    Issue-ID: APPC-1581
    Signed-off-by: Taka Cho <takamune.cho@att.com>

--- kubernetes/appc/resources/config/appc/opt/onap/appc/bin/health_check.sh
+++ kubernetes/appc/resources/config/appc/opt/onap/appc/bin/health_check.sh
@@ -13,11 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-startODL_status=$(ps -e | grep startODL | wc -l)
 waiting_bundles=$(/opt/opendaylight/current/bin/client bundle:list | grep Waiting | wc -l)
 run_level=$(/opt/opendaylight/current/bin/client system:start-level)
 
-  if [ "$run_level" == "Level 100" ] && [ "$startODL_status" -lt "1" ] && [ "$waiting_bundles" -lt "1" ]
+  if [ "$run_level" == "Level 100" ] && [ "$waiting_bundles" -lt "1" ]
   then
     echo APPC is healthy.
   else
