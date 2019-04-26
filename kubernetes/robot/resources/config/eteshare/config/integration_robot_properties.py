8f5004939a58a7a3d7c2398678a449a1767debe9
vrvarma <vv8305@att.com>
2019-04-25 23:08:01 -0400

    Configuration changes to make osdf ssl enabled
    Add common_config.yaml file to enable SSL
    Add the cert and key files
    Modify the deployment to mount the cert files
    Increment the docker image version
    Securing HAS api with https
    Modifying the health check protocol to https and credentials
    Updating certs to add has hostnames
    
    Change-Id: I860a0c436234b31d675ca0447fa92d511be0b140
    Signed-off-by: vrvarma <vv8305@att.com>
    Issue-ID: OPTFRA-293

--- kubernetes/robot/resources/config/eteshare/config/integration_robot_properties.py
+++ kubernetes/robot/resources/config/eteshare/config/integration_robot_properties.py
@@ -120,11 +120,14 @@ GLOBAL_MSO_PASSWORD = '{{ .Values.soPassword }}'
 GLOBAL_MUSIC_SERVER_PROTOCOL = "http"
 GLOBAL_MUSIC_SERVER_PORT = "8080"
 # oof global info - everything is from the private oam network (also called onap private network)
-GLOBAL_OOF_SERVER_PROTOCOL = "http"
+GLOBAL_OOF_SERVER_PROTOCOL = "https"
 # oof-homing info - everything is from the private oam network (also called onap private network)
 GLOBAL_OOF_HOMING_SERVER_PORT = "8091"
 # oof-sniro info - everything is from the private oam network (also called onap private network)
 GLOBAL_OOF_SNIRO_SERVER_PORT = "8698"
+#oof user
+GLOBAL_OOF_OSDF_USERNAME="{{ .Values.oofUsername }}"
+GLOBAL_OOF_OSDF_PASSWORD="{{ .Values.oofPassword }}"
 # oof cmso global info - everything is from the private oam network (also called onap private network)
 GLOBAL_OOF_CMSO_PROTOCOL = "https"
 GLOBAL_OOF_CMSO_SERVER_PORT = "8080"
