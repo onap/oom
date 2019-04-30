c34a77b96de4cb455dc30cde7f7bc70989a3699e
Jerry Flood <jflood@att.com>
2019-04-30 15:30:04 -0400

    Update CMSO OOM to use HTTPS and AAF
    Fixes OPTFRA-484 and OPTFRA-390
    
    Issue-ID: OPTFRA-391
    
    Change-Id: I2b65743e38464aac8dbc8cf81419e9ce36769b7d
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ kubernetes/oof/charts/oof-cmso/charts/oof-cmso-service/resources/config/mock.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask import request
+from flask import Response
+from flask import json
+from flask import send_from_directory
+import requests
+from threading import Thread
+import time
+
+import os
+import fnmatch
+import re
+import time
+import datetime
+
+app = Flask(__name__)
+ROOT_MOCK_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR =   os.path.join(ROOT_MOCK_DIR, "data")
+global requestNum
+requestNum = 1
+
+########################################################################
+########################################################################
+@app.route('/onap/so/infra/orchestrationRequests/v7/schedule/<VNFNAME>', methods=['GET', 'POST'])
+def soSchedule(VNFNAME):
+    if request.method == 'POST':
+        testid = request.headers.environ["HTTP_X_TRANSACTIONID"]
+        response = {
+            "status" : "202",
+            "entity" : {
+                "requestReferences" : {
+                    "requestId" : "000001"
+                    }
+                }
+            }
+        resp = Response(json.dumps(response), 200, mimetype='application/json')
+        return resp
+
+
+    else :
+        return "Helloooooo!!!!"
+
+########################################################################
+########################################################################
+@app.route('/onap/so/infra/orchestrationRequests/v7/<REQUESTID>', methods=['GET'])
+def soStatus(REQUESTID):
+    response = {"request" : { "requestStatus" : {
+        "requestState" : "COMPLETE",
+        "statusMessage" : "Done.",
+        "percentProgress" : 100,
+        "finishTime" : ""
+        }}}
+    now = datetime.datetime.utcnow()
+    #response["finishTime"] = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    response["request"]["requestStatus"]["finishTime"] = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    resp = Response(json.dumps(response), 200, mimetype='application/json')
+
+    return resp
+
+########################################################################
+########################################################################
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0',port=5000)
+    #app.run()
+
