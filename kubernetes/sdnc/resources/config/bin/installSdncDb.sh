07b864846d1bdaf9b28accf30040fb2aaba60d1d
Timoney, Dan (dt5972) <dtimoney@att.com>
2019-06-26 16:16:52 -0400

    Initialize SDNC DB from init container
    Updated sdnc helm chart to initialize database from an init container
    instead of initializing in startup script.
    
    Change-Id: I4133c70a5805227d89eef84e46bea8555bf8133d
    Issue-ID: SDNC-797
    Signed-off-by: Timoney, Dan (dt5972) <dtimoney@att.com>

--- kubernetes/sdnc/resources/config/bin/installSdncDb.sh
+++ kubernetes/sdnc/resources/config/bin/installSdncDb.sh
@@ -21,9 +21,11 @@
 # ============LICENSE_END=========================================================
 ###
 
-SDNC_HOME=${SDNC_HOME:-/opt/onap/sdnc}
-MYSQL_HOST=${MYSQL_HOST:-{{.Values.config.mariadbGalera.serviceName}}.{{.Release.Namespace}}}
-MYSQL_PASSWD=${MYSQL_PASSWD:-{{.Values.config.dbRootPassword}}}
+SDNC_ROOT=${SDNC_ROOT:-/opt/onap/sdnc}
+ETC_DIR=${ETC_DIR:-${SDNC_ROOT}/data}
+BIN_DIR=${BIN_DIR-${SDNC_ROOT}/bin}
+MYSQL_HOST=${MYSQL_HOST:-dbhost}
+MYSQL_PASSWD=${MYSQL_PASSWD:-openECOMP1.0}
 
 SDNC_DB_USER=${SDNC_DB_USER:-sdnctl}
 SDNC_DB_PASSWD=${SDNC_DB_PASSWD:-gamma}
@@ -32,7 +34,7 @@ SDNC_DB_DATABASE=${SDN_DB_DATABASE:-sdnctl}
 
 # Create tablespace and user account
 mysql -h ${MYSQL_HOST} -u root -p${MYSQL_PASSWD} mysql <<-END
-CREATE DATABASE ${SDNC_DB_DATABASE};
+CREATE DATABASE IF NOT EXISTS ${SDNC_DB_DATABASE};
 CREATE USER '${SDNC_DB_USER}'@'localhost' IDENTIFIED BY '${SDNC_DB_PASSWD}';
 CREATE USER '${SDNC_DB_USER}'@'%' IDENTIFIED BY '${SDNC_DB_PASSWD}';
 GRANT ALL PRIVILEGES ON ${SDNC_DB_DATABASE}.* TO '${SDNC_DB_USER}'@'localhost' WITH GRANT OPTION;
@@ -41,18 +43,27 @@ commit;
 END
 
 # load schema
-if [ -f ${SDNC_HOME}/data/sdnctl.dump ]
+if [ -f ${ETC_DIR}/sdnctl.dump ]
 then
   mysql -h ${MYSQL_HOST} -u root -p${MYSQL_PASSWD} sdnctl < ${SDNC_HOME}/data/sdnctl.dump
 fi
 
-for datafile in ${SDNC_HOME}/data/*.data.dump
+for datafile in ${ETC_DIR}/*.data.dump
 do
   mysql -h ${MYSQL_HOST} -u root -p${MYSQL_PASSWD} sdnctl < $datafile
 done
 
 # Create VNIs 100-199
-${SDNC_HOME}/bin/addVnis.sh 100 199
+${BIN_DIR}/addVnis.sh 100 199
 
 # Drop FK_NETWORK_MODEL foreign key as workaround for SDNC-291.
-${SDNC_HOME}/bin/rmForeignKey.sh NETWORK_MODEL FK_NETWORK_MODEL
+${BIN_DIR}/rmForeignKey.sh NETWORK_MODEL FK_NETWORK_MODEL
+
+if [ -x ${SDNC_HOME}/svclogic/bin/install.sh ]
+then
+    echo "Installing directed graphs"
+     ${SDNC_HOME}/svclogic/bin/install.sh
+fi
+
+
+exit 0
