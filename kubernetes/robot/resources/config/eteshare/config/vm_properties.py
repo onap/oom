b7b8506978356afdf041a7b2179c1b7ad357cd88
Michal Ptacek <m.ptacek@partner.samsung.com>
2019-07-08 05:29:33 +0000

    Renaming drools related variable
    This commit is a part of refactor for policy component adjustments.
    POLICY_HEALTHCHECK is not a good name for DROOLS components.
    This commit is fixing that.
    
    Change-Id: I38aa665460e0e3b87a253ce227b18eb71c1c1671
    Issue-ID: TEST-168
    Signed-off-by: Michal Ptacek <m.ptacek@partner.samsung.com>

--- kubernetes/robot/resources/config/eteshare/config/vm_properties.py
+++ kubernetes/robot/resources/config/eteshare/config/vm_properties.py
@@ -77,7 +77,7 @@ GLOBAL_INJECTED_OPENSTACK_USER_DOMAIN_REGION_THREE = '{{ .Values.openStackDomain
 GLOBAL_INJECTED_OPENSTACK_OAM_NETWORK_CIDR_PREFIX = '{{ .Values.openStackOamNetworkCidrPrefix }}'
 GLOBAL_INJECTED_OPENSTACK_PUBLIC_NETWORK = 'public'
 GLOBAL_INJECTED_POLICY_IP_ADDR = 'pdp.{{include "common.namespace" .}}'
-GLOBAL_INJECTED_POLICY_HEALTHCHECK_IP_ADDR = 'drools.{{include "common.namespace" .}}'
+GLOBAL_INJECTED_POLICY_DROOLS_IP_ADDR = 'drools.{{include "common.namespace" .}}'
 GLOBAL_INJECTED_PORTAL_IP_ADDR = 'portal-app.{{include "common.namespace" .}}'
 GLOBAL_INJECTED_POLICY_API_IP_ADDR = 'policy-api.{{include "common.namespace" .}}'
 GLOBAL_INJECTED_POLICY_PAP_IP_ADDR = 'policy-pap.{{include "common.namespace" .}}'
@@ -170,7 +170,7 @@ GLOBAL_INJECTED_PROPERTIES = {
     "GLOBAL_INJECTED_OPENSTACK_OAM_NETWORK_CIDR_PREFIX" : '{{ .Values.openStackOamNetworkCidrPrefix }}',
     "GLOBAL_INJECTED_OPENSTACK_PUBLIC_NETWORK" : 'public',
     "GLOBAL_INJECTED_POLICY_IP_ADDR" : 'pdp.{{include "common.namespace" .}}',
-    "GLOBAL_INJECTED_POLICY_HEALTHCHECK_IP_ADDR" : 'drools.{{include "common.namespace" .}}',
+    "GLOBAL_INJECTED_POLICY_DROOLS_IP_ADDR" : 'drools.{{include "common.namespace" .}}',
     "GLOBAL_INJECTED_POLICY_API_IP_ADDR" : 'policy-api.{{include "common.namespace" .}}',
     "GLOBAL_INJECTED_POLICY_PAP_IP_ADDR" : 'policy-pap.{{include "common.namespace" .}}',
     "GLOBAL_INJECTED_POLICY_DISTRIBUTION_IP_ADDR" : 'policy-distribution.{{include "common.namespace" .}}',
