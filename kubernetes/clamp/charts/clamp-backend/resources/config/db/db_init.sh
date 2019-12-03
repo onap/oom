3ff4af2e414b07650261155fdff506e2ef1057a8
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2019-12-03 09:54:23 +0100

    [CLAMP] Use mariadb galera common chart
    Instead of using "own" mariadb chart, reuse chart placed in common part
    of OOM installation.
    Will ease move to common galera cluster (which is not done by this
    commit).
    
    I didn't took specific conf of mariadb, please say if something is
    important in it.
    
    Change-Id: I93de3df09445b7cdfd8211c8321d80e03eea4167
    Issue-ID: OOM-1219
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>

--- kubernetes/clamp/charts/clamp-backend/resources/config/db/db_init.sh
+++ kubernetes/clamp/charts/clamp-backend/resources/config/db/db_init.sh
@@ -20,9 +20,8 @@
 # limitations under the License.
 # ============LICENSE_END============================================
 # ===================================================================
-# 
+#
 ###
 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -f < /docker-entrypoint-initdb.d/bulkload/create-db.sql
-## New model creation 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -f cldsdb4 < /docker-entrypoint-initdb.d/bulkload/create-tables.sql
+## New model creation
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} -P${MYSQL_PORT} < create-tables.sql
