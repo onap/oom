5233c46ce4a557d26f078925e075e778e50e7e51
tringuyen <tri.nguyen@tatacommunications.com>
2020-04-01 17:51:06 +0000

    [COMMON] add pre upgrade script for mariadb-galera
    When upgrading from a version to another, it may be unpossible to do it
    "simply" because of changes in unmutable properties of statefulsets. We
    change that here by creating a temporary deployment which will hold the
    whole databases during the time the old statefulset gets destroyed and
    the new one gets created.
    
    Issue-ID: OOM-2316
    Signed-off-by: tringuyen <tri.nguyen@tatacommunications.com>
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I318d72830d5002f50597e23e0753e292f8b47c53

---
+++ kubernetes/common/mariadb-galera/resources/post-upgrade-script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TEMP_POD=$(kubectl get pod -n $NAMESPACE_ENV --selector \
+  app='{{ include "common.fullname" . }}' -o \
+  jsonpath='{.items[?(@.metadata.ownerReferences[].kind=="ReplicaSet")].metadata.name}')
+
+tmp_MYSQL_USER=$(kubectl exec -n $NAMESPACE_ENV $TEMP_POD -- printenv \
+   MYSQL_USER | base64)
+
+tmp_MYSQL_PASSWORD=$(kubectl exec -n $NAMESPACE_ENV $TEMP_POD -- printenv \
+   MYSQL_PASSWORD | base64)
+
+tmp_ROOT_PASSWORD=$(kubectl exec -n $NAMESPACE_ENV $TEMP_POD -- printenv \
+   MYSQL_ROOT_PASSWORD | base64)
+
+FLAG_EX_ROOT_SEC='{{ tpl (index .Values.secrets 0).externalSecret . }}'
+
+FLAG_EX_SEC='{{ tpl (index .Values.secrets 1).externalSecret . }}'
+
+if [[ ! $FLAG_EX_ROOT_SEC == "" ]]
+then
+  kubectl get secret $FLAG_EX_ROOT_SEC -o \
+    yaml | sed 's/password\: *.*/password\: '"$tmp_ROOT_PASSWORD"'/' \
+    | kubectl apply -f -
+else
+  kubectl get secret {{ include "common.fullname" . }}-db-root-password -o \
+    yaml | sed 's/password\: *.*/password\: '"$tmp_ROOT_PASSWORD"'/' \
+    | kubectl apply -f -
+fi
+
+if [[ ! $FLAG_EX_SEC == "" ]]
+then
+  kubectl get secret $FLAG_EX_SEC -o \
+    yaml | sed 's/password\: *.*/password\: '"$tmp_MYSQL_PASSWORD"'/' \
+    | kubectl apply -f -
+else
+  kubectl get secret {{ include "common.fullname" . }}-db-user-credentials -o \
+    yaml | sed 's/password\: *.*/password\: '"$tmp_MYSQL_PASSWORD"'/' \
+    | kubectl apply -f -
+fi
+
+kubectl delete pod -n $NAMESPACE_ENV {{ include "common.fullname" . }}-0 --now
