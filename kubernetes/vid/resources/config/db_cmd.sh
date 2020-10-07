1e38c23b6e715945bb2595a508350dbd79184ae6
RPMishra <rpmishra@aarnanetworks.com>
2020-10-07 12:09:01 +0530

    [COMMON/VID] Modified VID to use Shared DB.
    This change modifies VID to use the shared DB instance of mariadb.
    In addition, we create a mechanism in mariadb-init to allow a shared DB
    user to run a script after the DB has been created.
    The script can be passed as a configMap (as is the case with VID) or it
    can be a standalone script that is passed as a string.
    
    Issue-ID: OOM-1226
    Change-Id: Ie51823338562a6fbe7472025d62cd4dba688168d
    Signed-off-by: RPMishra <rpmishra@aarnanetworks.com>
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>

--- kubernetes/vid/resources/config/db_cmd.sh
+++ kubernetes/vid/resources/config/db_cmd.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 {{/*
 # Copyright © 2018 AT&T
+# Copyright © 2020 Aarna Networks
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +16,29 @@
 # limitations under the License.
 */}}
 
-echo "Going to run mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} -P${MYSQL_PORT} ..."
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} -P${MYSQL_PORT} < /db-config/vid-pre-init.sql
+DB=vid_openecomp_epsdk
+USER_VAR="MYSQL_USER_${DB^^}"
+PASS_VAR="MYSQL_PASSWORD_${DB^^}"
+MYSQL_USER=${!USER_VAR}
+MYSQL_PASSWORD=${!PASS_VAR}
+
+echo "Going to run mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${DB_HOST} -P${DB_PORT} ..."
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${DB_HOST} -P${DB_PORT} <<'EOD'
+CREATE TABLE IF NOT EXISTS `vid_openecomp_epsdk`.`schema_info` (
+`SCHEMA_ID` VARCHAR(25) NOT NULL,
+`SCHEMA_DESC` VARCHAR(75) NOT NULL,
+`DATASOURCE_TYPE` VARCHAR(100) NULL DEFAULT NULL,
+`CONNECTION_URL` VARCHAR(200) NOT NULL,
+`USER_NAME` VARCHAR(45) NOT NULL,
+`PASSWORD` VARCHAR(45) NULL DEFAULT NULL,
+`DRIVER_CLASS` VARCHAR(100) NOT NULL,
+`MIN_POOL_SIZE` INT(11) NOT NULL,
+`MAX_POOL_SIZE` INT(11) NOT NULL,
+`IDLE_CONNECTION_TEST_PERIOD` INT(11) NOT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+EOD
+
 if [ $? -ne 0 ];then
         echo "ERROR: Failed to run ${cmd} vid-pre-init.sql"
         exit 1
