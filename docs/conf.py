090941b572c917d8e84ea709eb3696fdc8f53be1
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2020-12-09 10:13:42 +0100

    [COMMON] Add doc linting
    Add 3 automated doc linting (sphinx building, linkchecking and doc8) in
    order to have an error free documentations
    Fix also issues found by these linters in order to start without errors.
    
    Issue-ID: OOM-2648
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I318718c956020412a120ba3caeb9e21d35a99833
    (cherry picked from commit ec07bca720e5e2156be085e89f1ce8521a602eaf)

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,30 @@ linkcheck_ignore = [
 
 intersphinx_mapping = {}
 
+needs_services = {
+    'github-issues': {
+        'url': 'https://api.github.com/',
+        'need_type': 'spec',
+        'max_amount': 2,
+        'max_content_lines': 20,
+        'id_prefix': 'GH_ISSUE_'
+    },
+    'github-prs': {
+        'url': 'https://api.github.com/',
+        'need_type': 'spec',
+        'max_amount': 2,
+        'max_content_lines': 20,
+        'id_prefix': 'GH_PR_'
+    },
+    'github-commits': {
+        'url': 'https://api.github.com/',
+        'need_type': 'spec',
+        'max_amount': 2,
+        'max_content_lines': 20,
+        'id_prefix': 'GH_COMMIT_'
+    }
+}
+
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
