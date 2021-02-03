1fe00a37582ea4332cee38c8cab709c206bfaab1
GuangrongFu <fu.guangrong@zte.com.cn>
2021-02-03 17:29:08 +0800

    [HOLMES] Migrated Holmes from Cloudify to Helm
    Initial Helm charts for Holmes components
    
    Change-Id: I6c97862898ab34b63a15b942b1396c0d19179e46
    Issue-ID: HOLMES-396
    Signed-off-by: GuangrongFu <fu.guangrong@zte.com.cn>
    Signed-off-by: vv770d <vv770d@att.com>

---
+++ kubernetes/holmes/components/holmes-engine-mgmt/resources/config/onap-holmes_engine-createobj.sql
@@ -0,0 +1,51 @@
+--
+-- Copyright 2017 ZTE Corporation.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+\c postgres
+
+/******************CREATE NEW DATABASE AND USER***************************/
+CREATE DATABASE ${DB_NAME};
+
+CREATE ROLE ${JDBC_USERNAME} with PASSWORD '${JDBC_PASSWORD}' LOGIN;
+
+\encoding UTF8;
+
+/******************DELETE OLD TABLE AND CREATE NEW***************************/
+\c ${DB_NAME};
+
+DROP TABLE IF EXISTS ALARM_INFO;
+
+CREATE TABLE ALARM_INFO (
+  EVENTID VARCHAR(150) NOT NULL,
+  EVENTNAME VARCHAR(150) NOT NULL,
+  ALARMISCLEARED SMALLINT NOT NULL,
+  ROOTFLAG SMALLINT NOT NULL,
+  STARTEPOCHMICROSEC BIGINT NOT NULL,
+  LASTEPOCHMICROSEC BIGINT NOT NULL,
+  SOURCEID VARCHAR(150)  NOT NULL,
+  SOURCENAME VARCHAR(150)  NOT NULL,
+  PRIMARY KEY (EVENTID)
+);
+
+CREATE TABLE IF NOT EXISTS ENGINE_ENTITY (
+  ID VARCHAR(150) NOT NULL,
+  IP VARCHAR(128) NOT NULL,
+  PORT SMALLINT NOT NULL,
+  LASTMODIFIED BIGINT NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+GRANT ALL PRIVILEGES ON ALARM_INFO TO ${JDBC_USERNAME};
+GRANT ALL PRIVILEGES ON ENGINE_ENTITY TO ${JDBC_USERNAME};
