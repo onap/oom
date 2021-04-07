0c9670e5a18241f2ed49433f56176708998ac706
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2021-04-07 17:22:42 +0200

    [COMMON][CERTINIT] Generate cert with certInit
    Some components are http based but want to be usable from outside world.
    Instead of dealing with TLS part on the component itself, let's use
    certInitializer to generate a secret with the certs which will be usable
    by Ingress.
    
    Issue-ID: SO-3078
    Issue-ID: SO-3237
    Issue-ID: CPS-281
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: If166716d159586b1eb94c111e9d3d82a54c2fd6e

--- kubernetes/common/certInitializer/resources/retrieval/retrieval_check.sh
+++ kubernetes/common/certInitializer/resources/retrieval/retrieval_check.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 {{/*
 # Copyright © 2021 Orange
 #
@@ -12,8 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-*/}}
-#!/bin/sh
+*/ -}}
 
 echo "*** retrieving passwords for certificates"
 export $(/opt/app/aaf_config/bin/agent.sh local showpass \
