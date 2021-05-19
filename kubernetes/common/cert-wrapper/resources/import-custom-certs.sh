30eff053ac6de072da95eba5bfb851235daf6bf8
a.sreekumar <ajith.sreekumar@bell.ca>
2021-05-19 12:52:14 +0100

    [COMMON] Change import-custom-cert from bash to sh
    As part of removing GPLv3 license, policy components have moved from
    bash to sh. This change is required in import-custom-certs script so
    that custom certificates can be imported into components that try to
    import them. Without this change, the init containers of the
    components will just fail.
    
    Change-Id: I6c5028428d4cd7c8baf3e96cb16a3cd91db57f9e
    Issue-ID: POLICY-3232
    Signed-off-by: a.sreekumar <ajith.sreekumar@bell.ca>
    (cherry picked from commit f79b6676cfdc380e004f184a21bb969b2824c06e)

--- kubernetes/common/cert-wrapper/resources/import-custom-certs.sh
+++ kubernetes/common/cert-wrapper/resources/import-custom-certs.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 {{/*
 
-# Copyright © 2020 Bell Canada
+# Copyright © 2020-2021 Bell Canada
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
