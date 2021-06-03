175c6d70e0cefb5d8221873444c5ae318403e726
Vijay Venkatesh Kumar <vv770d@att.com>
2021-06-03 16:51:33 -0400

    [OOM-DCAE] Chartmusuem deployment support
    Introduction of chartmusuem as internal repo for
    ONAP components to push/pull charts post instantiation
    + Script to preload charts to this repo
    
    Change-Id: I4880900548dfe1d3e47a67b3822f82a15314b5b7
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>
    Issue-ID: DCAEGEN2-2630
    Issue-ID: OOM-2734
    Issue-ID: INT-1895
    Issue-ID: DCAEGEN2-2694
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>

---
+++ kubernetes/contrib/tools/registry-initialize.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -x
+
+# Copyright (c) 2021 AT&T. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pre-requisite
+# 1. Chart packages available under local directory provided as input/argument
+# 2. helm client installed with push plugin
+# 3. ONAP chartmuseum service deployed
+
+if [ $# -eq 0 ]
+  then
+    echo "Chart Base directory must be provided as input!!"
+    echo "Usage: registry-initialize.sh <chart directory>"
+    exit 1
+fi
+
+
+BASEDIR=$1
+[ -z "$BASEDIR" ] && exit "Chart base directory provided $BASEDIR is empty"
+[[ $(ls -A $BASEDIR/*tgz) ]] && echo "$BASEDIR valid" || exit "No chart package on $BASEDIR provided"
+
+# Expose cluster port via port-forwarding
+kubectl -n onap port-forward service/chart-museum 27017:80 &
+if [ $? -ne 0 ]; then
+  echo "Error in portforwarding; registry cannot be added!!"
+  exit 1
+fi
+
+sleep 5
+
+# Add chartmuseum repo as helm repo
+# Credentials should match config defined in oom\kubernetes\platform\components\chartmuseum\values.yaml
+helm repo add k8s-registry http://127.0.0.1:27017 --username "onapintializer" --password "demo123456!"
+if [ $? -ne 0 ]; then
+   echo "registry cannot be added!!"
+   pkill -f "port-forward service/chart-museum"
+   exit 1
+fi
+
+# Initial scope is pushing only dcae charts
+# can be expanded to include all onap charts if required
+for file in $BASEDIR/dcae*tgz
+do
+ # use helm plugin to push charts
+ helm push $file k8s-registry
+ if [ $? -eq 0 ]; then
+  echo "$file uploaded to registry successfully"
+ else
+  echo "registry upload failed!!"
+  pkill -f "port-forward service/chart-museum"
+  helm repo remove k8s-registry
+  exit 1
+ fi
+done
+
+echo "All Helm charts successfully uploaded into internal repository"
+
+# Remove the port-forwarding process
+pkill -f "port-forward service/chart-museum"
+
+# Remove helm registry from local
+helm repo remove k8s-registry
