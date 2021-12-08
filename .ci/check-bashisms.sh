765f4efe648b31ca9668c407d4f29b7af649ad44
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2021-12-08 12:59:32 +0100

    [GLOBAL] Move to Gitlab
    Add all necessary files in order to move OOM repo to gitlab and also
    uses gitlab-ci features.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: Ie3ba8d22828b103219d590886a8c58825eff2eef

--- .ci/check-bashisms.sh
+++ .ci/check-bashisms.sh
@@ -14,14 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -eu
+
 if ! which checkbashisms >/dev/null && ! sudo yum install devscripts-minimal && ! sudo apt-get install devscripts
 then
     printf "checkbashisms command not found - please install it \n\
             (e.g. sudo apt-get install devscripts | yum install devscripts-minimal )\n" >&2
     exit 2
 fi
+
 find . -not -path '*/.*' -name '*.sh' -exec checkbashisms {} + || exit 3
 find . -not -path '*/.*' -name '*.failover' -exec checkbashisms -f \{\} + || exit 4
 ! find . -not -path '*/.*' -name '*.sh' -exec grep 'local .*=' {} + || exit 5
 ! find . -not -path '*/.*' -name '*.failover' -exec grep 'local .*=' {} + || exit 6
+
 exit 0
