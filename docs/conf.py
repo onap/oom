8b41e6d8d651de4d2227c6b60899424f0987cb16
efiacor <fiachra.corcoran@est.tech>
2022-05-11 16:04:13 +0100

    [DOC] Refactor docs for Kohn release
    Refactoring docs for K
    Adding nexus chart deploy
    
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I8047255feb4d9f8d858ac2d13a6be6f1d93e00fd
    Issue-ID: OOM-3028

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,57 @@
-from docs_conf.conf import *
+project = "onap"
+release = "master"
+version = "master"
+
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+  "style_nav_header_background": "white",
+  "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.swaggerdoc',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.plantuml',
+    'sphinxcontrib.spelling'
+    ]
+
+#
+# Map to 'latest' if this file is used in 'latest' (master) 'doc' branch.
+# Change to {releasename} after you have created the new 'doc' branch.
+#
 
 branch = 'latest'
-master_doc = 'index'
-
-linkcheck_ignore = [
-    'http://localhost',
-]
 
 intersphinx_mapping = {}
+doc_url = 'https://docs.onap.org/projects'
+master_doc = 'index'
+
+exclude_patterns = ['.tox']
+
+linkcheck_ignore = [
+    "https://nexus3.onap.org"
+]
+
+spelling_word_list_filename='spelling_wordlist.txt'
+spelling_lang = "en_GB"
+
+#
+# Example:
+# intersphinx_mapping['onap-aai-aai-common'] = ('{}/onap-aai-aai-common/en/%s'.format(doc_url) % branch, None)
+#
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
-    app.add_css_file("css/ribbon_onap.css")
+    app.add_css_file("css/ribbon.css")
