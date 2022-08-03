db355bf65937b083e494d9ef273ce6cae19200cf
dujunyi <hunterdevil@foxmail.com>
2022-08-03 13:10:19 +0800

    [UUI] solve 2 components in one Docker
    1.add postgres env in uui-server
    2.add server init sql in uui-postgres
    
    Issue-ID: USECASEUI-405
    Signed-off-by: dujunyi <hunterdevil@foxmail.com>
    Change-Id: I6868b529a79938d85c00eddb85c0aa009aad4c84

--- kubernetes/uui/components/uui-server/resources/entrypoint/run.sh
+++ kubernetes/uui/components/uui-server/resources/entrypoint/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-{{/*
 #
 # Copyright 2016-2017 ZTE Corporation.
-# Copyright 2021 Orange
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,20 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-*/}}
 
 main_path="/home/uui"
 echo @main_path@ $main_path
-
-echo "Starting postgreSQL..."
-#service postgresql start
-postmaster -D /usr/share/postgresql/data &
-sleep 10
-
-echo "usecase-ui database init script start..."
-dbScript="$main_path/resources/bin/initDB.sh"
-$dbScript 127.0.0.1 5432 postgres uui
-echo "usecase-ui database init script finished normally..."
 
 JAVA_PATH="$JAVA_HOME/bin/java"
 JAVA_OPTS="-Xms50m -Xmx128m"
@@ -38,6 +25,7 @@ echo @JAVA_OPTS@ $JAVA_OPTS
 
 jar_path="$main_path/usecase-ui-server.jar"
 echo @jar_path@ $jar_path
+
 
 echo "Starting usecase-ui-server..."
 $JAVA_PATH $JAVA_OPTS -classpath $jar_path -jar $jar_path $SPRING_OPTS
