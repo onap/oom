e1441a7fa307a5e6fdee64d7fd41b30cf85eb6f2
xudan16 <xudan16@huawei.com>
2023-05-25 10:38:28 +0800

    [UUI] Update database sql of uui-intent-analysis
    update file intent-analysis-init.sql to update the
    initialization of uui-intent-analysis DB.
    
    Issue-ID: OOM-3182
    Signed-off-by: xudan16 <xudan16@huawei.com>
    Change-Id: I30e9920afb3c881d7ecd6f41f8a4d0900d22b8fc

--- kubernetes/uui/components/uui-intent-analysis/resources/config/intent-analysis-init.sql
+++ kubernetes/uui/components/uui-intent-analysis/resources/config/intent-analysis-init.sql
@@ -21,7 +21,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table if not exists intent(
     intent_id varchar(255) primary key,
-    intent_name varchar(255)
+    intent_name varchar(255),
+    intent_generateType VARCHAR (225)
 );
 
 create table if not exists expectation(
@@ -89,3 +90,21 @@ create table if not exists intent_management_function_reg_info(
     handle_name varchar(255),
     intent_function_type varchar(255)
     );
+
+create table if not exists intent_event_record(
+    id varchar(255) DEFAULT uuid_generate_v4 (),
+    intent_id varchar(255),
+    intent_name varchar(255),
+    intent_status varchar (225),
+    operate_type varchar (225),
+    parent_id varchar(255)
+    );
+
+-- ----------------------------
+-- Records of intent_management_function_reg_info
+-- ----------------------------
+
+insert into intent_management_function_reg_info(imfr_info_id, imfr_info_description, support_area, support_model, support_interfaces, handle_name, intent_function_type) select 'CLLBusinessId','CLLBusiness','CLLBUSINESS',null,'CREATE,DELETE,UPDATE,SEARCH','CLLBusinessIntentManagementFunction','INTERNALFUNCTION'  where not exists(select * from intent_management_function_reg_info where imfr_info_id='CLLBusinessId' )
+insert into intent_management_function_reg_info(imfr_info_id, imfr_info_description, support_area, support_model, support_interfaces, handle_name, intent_function_type) select 'CLLDeliveryId','CLLDelivery','CLLBUSINESS,DELIVERY',null,'CREATE,DELETE,UPDATE,SEARCH','CLLDeliveryIntentManagementFunction','INTERNALFUNCTION'  where not exists(select * from intent_management_function_reg_info where imfr_info_id='CLLDeliveryId' )
+insert into intent_management_function_reg_info(imfr_info_id, imfr_info_description, support_area, support_model, support_interfaces, handle_name, intent_function_type) select 'CLLAssuranceId','CLLAssurance','CLLBUSINESS,ASSURANCE',null,'CREATE,DELETE,UPDATE,SEARCH','CLLAssuranceIntentManagementFunction','INTERNALFUNCTION'  where not exists(select * from intent_management_function_reg_info where imfr_info_id='CLLAssuranceId' )
+
