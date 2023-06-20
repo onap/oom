2e7bfbd6563e614285417a1bca5c7634302c30d7
Andreas Geissler <andreas-geissler@telekom.de>
2023-06-20 11:38:39 +0200

    [OOM] fixing k8s resources and limits
    This fix is adjusting OOM helm charts components resources limits and requests
    
    Issue-ID: OOM-3199
    
    Signed-off-by: Vladimir Turok <vladimir.turok@t-systems.com>
    Change-Id: I56aeba925cda4984277ac7bbf23cfd158263d30f

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 project = "onap"
-release = "kohn"
-version = "kohn"
+release = "master"
+version = "master"
 
 author = "Open Network Automation Platform"
 # yamllint disable-line rule:line-length
@@ -32,7 +32,7 @@ exclude_patterns = ['archived/*.rst', '.tox']
 # Change to {releasename} after you have created the new 'doc' branch.
 #
 
-branch = 'kohn'
+branch = 'master'
 
 intersphinx_mapping = {}
 doc_url = 'https://docs.onap.org/projects'
