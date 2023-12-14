43d6f2f39dac316e7203578675099cdcb4c3575b
Andreas Geissler <andreas-geissler@telekom.de>
2023-12-14 09:01:47 +0100

    [DOC] Update conf.py and set ONAP release versions
    Update the conf.py to set the right release and
    added fixed component versions in ONAP Chart.yaml
    
    Issue-ID: OOM-3242
    
    Change-Id: I985efca6254b1815d50d1c92eb805bb08c651a89
    Signed-off-by: Andreas Geissler <andreas-geissler@telekom.de>

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 project = "onap"
-release = "master"
-version = "master"
+release = "montreal"
+version = "montreal"
 
 author = "Open Network Automation Platform"
 # yamllint disable-line rule:line-length
@@ -32,7 +32,7 @@ exclude_patterns = ['archived/*.rst', '.tox']
 # Change to {releasename} after you have created the new 'doc' branch.
 #
 
-branch = 'master'
+branch = 'montreal'
 
 intersphinx_mapping = {}
 doc_url = 'https://docs.onap.org/projects'
