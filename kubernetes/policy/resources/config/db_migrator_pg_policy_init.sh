f7ae955675b9b3a2710ddefd00fc5b223a9ec83c
saul.gill <saul.gill@est.tech>
2024-03-07 12:43:01 +0000

    [POLICY] Fix postgres support and SASL
    Charts will allow mariadb or postgres
    Allow configuration of db choice globally
    Deleted jobs that aren't needed
    Altered db migrator script
    Allow SASL config in drools
    Drools chart fixed
    Removed readiness jobs for kafka
    Exposed more config for drools
    
    Issue-ID: POLICY-4943
    Change-Id: I26c4a7c1adc2d615602e3e6495ecedfb017ef39e
    Signed-off-by: saul.gill <saul.gill@est.tech>

--- kubernetes/policy/resources/config/db_migrator_pg_policy_init.sh
+++ kubernetes/policy/resources/config/db_migrator_pg_policy_init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 {{/*
-# Copyright (C) 2022 Nordix Foundation.
+# Copyright (C) 2022, 2024 Nordix Foundation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 */}}
-/opt/app/policy/bin/prepare_upgrade.sh ${SQL_DB}
-/opt/app/policy/bin/db-migrator-pg -s ${SQL_DB} -o upgrade
-rc=$?
-/opt/app/policy/bin/db-migrator-pg -s ${SQL_DB} -o report
-exit $rc
+
+for schema in ${SQL_DB}; do
+    echo "Initializing $schema..."
+    /opt/app/policy/bin/prepare_upgrade.sh ${schema}
+
+    /opt/app/policy/bin/db-migrator-pg -s ${schema} -o report
+
+    /opt/app/policy/bin/db-migrator-pg -s ${schema} -o upgrade
+    rc=$?
+
+    /opt/app/policy/bin/db-migrator-pg -s ${schema} -o report
+
+    if [ "$rc" != 0 ]; then
+        break
+    fi
+done
